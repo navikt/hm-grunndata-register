77a805d09b60c1b87a980590b015934b38bfac98
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2022-11-30 13:32:34 +0100

    rename to ProductRegistration

--- src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistration.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistration.kt
@@ -7,6 +7,7 @@ import io.micronaut.data.annotation.TypeDef
 import io.micronaut.data.model.DataType
 import java.time.LocalDateTime
 import java.util.*
+import javax.persistence.Column
 
 @MappedEntity("product_reg_v1")
 data class ProductRegistration(
@@ -14,8 +15,9 @@ data class ProductRegistration(
     @field:Id
     var id: Long = -1L,
     val uuid: UUID,
-    val supplierUUID: UUID,
+    val supplier_uuid: UUID,
     val supplierRef: String,
+    @field:Column(name="hms_artnr")
     val HMSArtNr: String?,
     val title: String,
     val draft: DraftStatus = DraftStatus.DRAFT,
@@ -25,6 +27,8 @@ data class ProductRegistration(
     val adminInfo: AdminInfo?,
     val created: LocalDateTime = LocalDateTime.now(),
     val updated: LocalDateTime = LocalDateTime.now(),
+    val published: LocalDateTime?,
+    val expired: LocalDateTime?,
     val createdBy: String,
     val updatedBy: String,
     val createdByAdmin: Boolean = false,
