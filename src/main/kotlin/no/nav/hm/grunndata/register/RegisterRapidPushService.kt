eb4e8ec621feb59b1351242e585d2388044841a6
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-16 15:33:30 +0100

    dto events

--- src/main/kotlin/no/nav/hm/grunndata/register/RegisterRapidPushService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/RegisterRapidPushService.kt
@@ -5,6 +5,7 @@ import no.nav.hm.grunndata.rapid.dto.RapidDTO
 import no.nav.hm.grunndata.rapid.dto.rapidDTOVersion
 import no.nav.hm.grunndata.register.product.REGISTER
 import no.nav.hm.rapids_rivers.micronaut.RapidPushService
+import java.time.LocalDateTime
 
 @Singleton
 class RegisterRapidPushService(private val kafkaRapidService: RapidPushService) {
@@ -13,7 +14,7 @@ class RegisterRapidPushService(private val kafkaRapidService: RapidPushService) 
         kafkaRapidService.pushToRapid(
             key = "$eventName-${dto.id}",
             eventName = eventName, payload = dto, keyValues = mapOf("createdBy" to REGISTER,
-                "dtoVersion" to rapidDTOVersion)
+                "created" to LocalDateTime.now(), "dtoVersion" to rapidDTOVersion)
         )
     }
 }
--- src/main/kotlin/no/nav/hm/grunndata/register/product/ProductSyncRiver.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductSyncRiver.kt
@@ -10,6 +10,7 @@ import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.River
 import no.nav.hm.grunndata.rapid.dto.DraftStatus
 import no.nav.hm.grunndata.rapid.dto.ProductDTO
+import no.nav.hm.grunndata.rapid.dto.rapidDTOVersion
 import no.nav.hm.grunndata.rapid.event.EventName
 import no.nav.hm.rapids_rivers.micronaut.RiverHead
 import org.slf4j.LoggerFactory
@@ -30,11 +31,14 @@ class ProductSyncRiver(river: RiverHead,
             .validate { it.demandValue("payloadType", ProductDTO::class.java.simpleName)}
             .validate { it.demandKey("payload")}
             .validate { it.demandKey("eventId")}
+            .validate { it.demandKey( "dtoVersion")}
             .register(this)
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         val eventId = packet["eventId"].asText()
+        val dtoVersion = packet["dtoVersion"].asLong()
+        if (dtoVersion > rapidDTOVersion) LOG.warn("dto version $dtoVersion is newer than $rapidDTOVersion")
         val dto = objectMapper.treeToValue(packet["payload"], ProductDTO::class.java)
         runBlocking {
             productRegistrationRepository.findById(dto.id)?.let { inDb ->
