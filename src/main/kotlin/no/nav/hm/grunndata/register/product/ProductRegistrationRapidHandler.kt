ac0d7192f1d0fcb7c8043d69390f483ed7ca1277
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-16 08:28:33 +0100

    use latest dtos

--- src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationRapidHandler.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationRapidHandler.kt
@@ -1,6 +1,7 @@
 package no.nav.hm.grunndata.register.product
 
 import jakarta.inject.Singleton
+import no.nav.hm.grunndata.dto.AdminStatus
 import no.nav.hm.grunndata.dto.DraftStatus
 import no.nav.hm.grunndata.dto.ProductRegistrationDTO
 import no.nav.hm.grunndata.register.productRegistrationEventName
@@ -10,7 +11,7 @@ import no.nav.hm.rapids_rivers.micronaut.RapidPushService
 open class ProductRegistrationRapidHandler(private val kafkaRapidService: RapidPushService) {
 
     fun pushProductToKafka(dto: ProductRegistrationDTO) {
-        if (dto.draftStatus == DraftStatus.DONE) {
+        if (dto.draftStatus == DraftStatus.DONE && dto.adminStatus == AdminStatus.APPROVED) {
             kafkaRapidService.pushToRapid(
                 key = "$productRegistrationEventName-${dto.id}",
                 eventName = productRegistrationEventName, payload = dto
