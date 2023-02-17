084bd7a5e28a9c1ec2f8292fa7d89e3131fe1bf1
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-17 11:35:35 +0100

    cache suppliers

--- src/main/kotlin/no/nav/hm/grunndata/register/supplier/SupplierRepository.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/supplier/SupplierRepository.kt
@@ -1,9 +1,21 @@
 package no.nav.hm.grunndata.register.supplier
 
+import io.micronaut.cache.annotation.CacheConfig
+import io.micronaut.cache.annotation.Cacheable
 import io.micronaut.data.jdbc.annotation.JdbcRepository
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.kotlin.CoroutineCrudRepository
 import java.util.UUID
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
-interface SupplierRepository: CoroutineCrudRepository<Supplier, UUID>
+@CacheConfig("suppliers")
+interface SupplierRepository: CoroutineCrudRepository<Supplier, UUID> {
+
+    @Cacheable
+    suspend fun findByIdentifier(identifier: String): Supplier?
+
+    @Cacheable
+    override suspend fun findById(id: UUID): Supplier?
+
+
+}
