3821f0f1d9b1ef324f3b62d448bf5b59c6cf639f
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-03-30 14:35:35 +0200

    upgrade micronaut 3.8.7 and dep versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,17 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
-val micronautVersion="3.8.6"
+val micronautVersion="3.8.7"
 val kafkaVersion = "3.2.1"
 val micrometerRegistryPrometheusVersion = "1.9.1"
 val junitJupiterVersion = "5.9.0"
-val logbackClassicVersion = "1.4.5"
-val logbackEncoderVersion = "7.2"
+val logbackClassicVersion = "1.4.6"
+val logbackEncoderVersion = "7.3"
 val kafkaEmbeddedVersion = "3.2.1"
-val postgresqlVersion= "42.5.1"
+val postgresqlVersion= "42.5.4"
 val tcVersion= "1.17.6"
-val mockkVersion = "1.13.2"
-val kotestVersion = "5.5.4"
+val mockkVersion = "1.13.4"
+val kotestVersion = "5.5.5"
 val rapidsRiversVersion="202303011052"
 val grunndataDtoVersion = "202303301323"
 
@@ -22,10 +22,9 @@ version = properties["version"] ?: "local-build"
 plugins {
     kotlin("jvm") version "1.7.0"
     kotlin("kapt") version "1.7.0"
-    kotlin("plugin.allopen") version "1.7.0"
     id("java")
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("io.micronaut.application") version "3.7.2"
+    id("io.micronaut.application") version "3.7.4"
 }
 
 configurations.all {
@@ -65,7 +64,7 @@ dependencies {
     testImplementation("io.micronaut.test:micronaut-test-kotest5")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
     testImplementation("org.testcontainers:postgresql:${tcVersion}")
 
     // Rapids and Rivers
