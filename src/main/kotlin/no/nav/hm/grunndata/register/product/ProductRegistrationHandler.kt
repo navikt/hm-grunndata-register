5851d261d291e17c820b2031e4f4c8e42aca4998
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-04-03 08:58:58 +0200

    map product data to dto

--- src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationHandler.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationHandler.kt
@@ -2,32 +2,26 @@ package no.nav.hm.grunndata.register.product
 
 import io.micronaut.security.authentication.Authentication
 import jakarta.inject.Singleton
+import kotlinx.coroutines.runBlocking
 import no.nav.hm.grunndata.rapid.dto.*
+import no.nav.hm.grunndata.rapid.event.EventName
+import no.nav.hm.grunndata.register.RegisterRapidPushService
+import no.nav.hm.grunndata.register.supplier.SupplierRepository
+import no.nav.hm.grunndata.register.supplier.toDTO
 import java.time.LocalDateTime
 import java.util.*
 
 @Singleton
-class ProductRegistrationHandler {
+class ProductRegistrationHandler(private val registerRapidPushService: RegisterRapidPushService,
+                                 private val supplierRepository: SupplierRepository) {
 
     fun makeTemplateOf(registration: ProductRegistration, authentication: Authentication): ProductRegistrationDTO {
         val productId = UUID.randomUUID()
-        val product = registration.toDTO().productDTO.copy(
-            id = productId,
-            status = ProductStatus.INACTIVE,
-            hmsArtNr=null,
-            identifier = productId.toString(),
-            supplierRef = productId.toString(),
-            created = LocalDateTime.now(),
-            updated = LocalDateTime.now(),
-            published = LocalDateTime.now(),
-            expired = LocalDateTime.now().plusYears(10) ,
-            createdBy = "REGISTER",
-            updatedBy = "REGISTER"
-        )
         return registration.toDTO().copy(
             id = productId,
             draftStatus =  DraftStatus.DRAFT,
             adminStatus = AdminStatus.PENDING,
+            registrationStatus = RegistrationStatus.ACTIVE,
             message = null,
             adminInfo = null,
             created = LocalDateTime.now(),
@@ -36,11 +30,72 @@ class ProductRegistrationHandler {
             expired = LocalDateTime.now().plusYears(10),
             updatedByUser = authentication.name,
             createdByUser = authentication.name,
-            createdBy = product.createdBy,
-            updatedBy = product.updatedBy,
-            createdByAdmin = authentication.isAdmin(),
-            productDTO = product
+            createdByAdmin = authentication.isAdmin()
         )
     }
+
+    fun pushToRapidIfNotDraft(dto: ProductRegistrationDTO) {
+        runBlocking {
+            if (dto.draftStatus == DraftStatus.DONE) {
+                val rapidDTO = dto.toRapidDTO()
+                registerRapidPushService.pushDTOToKafka(rapidDTO, EventName.registeredProductV1)
+            }
+        }
+    }
+
+    suspend fun ProductRegistrationDTO.toRapidDTO() = ProductRegistrationRapidDTO (
+        id = id,
+        draftStatus = draftStatus,
+        adminStatus = adminStatus,
+        registrationStatus = registrationStatus,
+        message = message,
+        adminInfo = adminInfo,
+        created = created,
+        updated = updated,
+        published = published,
+        expired = expired,
+        updatedByUser = updatedByUser,
+        createdByUser = createdByUser,
+        createdBy = createdBy,
+        updatedBy = updatedBy,
+        createdByAdmin = createdByAdmin,
+        version = version,
+        productDTO = productData.toProductDTO(this),
+    )
+
+    suspend fun ProductData.toProductDTO(registration: ProductRegistrationDTO): ProductDTO = ProductDTO (
+        id = registration.id,
+        supplier = supplierRepository.findById(registration.supplierId)!!.toDTO(),
+        supplierRef = registration.supplierRef,
+        title =  registration.title,
+        articleName = registration.articleName,
+        hmsArtNr = registration.hmsArtNr,
+        identifier = registration.id.toString(),
+        isoCategory = isoCategory,
+        accessory = accessory,
+        sparePart = sparePart,
+        seriesId = seriesId,
+        techData = techData,
+        media = media,
+        created = registration.created,
+        updated = registration.updated,
+        published = registration.published ?: LocalDateTime.now(),
+        expired = registration.expired ?: LocalDateTime.now().plusYears(10),
+        agreementInfo = agreementInfo,
+        hasAgreement = agreementInfo!=null,
+        createdBy = registration.createdBy,
+        updatedBy = registration.updatedBy,
+        attributes = attributes,
+        status = setCorrectStatusFor(registration)
+    )
+
+    private fun setCorrectStatusFor(registration: ProductRegistrationDTO): ProductStatus =
+        if (registration.registrationStatus == RegistrationStatus.DELETED
+                ||  registration.adminStatus != AdminStatus.APPROVED
+                ||  registration.draftStatus == DraftStatus.DRAFT
+                ||  LocalDateTime.now().isAfter(registration.expired))
+            ProductStatus.INACTIVE
+        else
+            ProductStatus.ACTIVE
 
 }
--- src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationMapper.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationMapper.kt
@@ -1,20 +1,19 @@
 package no.nav.hm.grunndata.register.product
 
-import no.nav.hm.grunndata.rapid.dto.ProductRegistrationDTO
-
-
-fun ProductRegistrationDTO.toEntity(): ProductRegistration = ProductRegistration(id = id,
-    supplierId = productDTO.supplier.id, supplierRef = productDTO.supplierRef, hmsArtNr = productDTO.hmsArtNr,
-    title = productDTO.title, articleName = productDTO.articleName, draftStatus = draftStatus,
-    adminStatus = adminStatus, registrationStatus = registrationStatus, message = message, adminInfo = adminInfo, created = created,
+fun ProductRegistrationDTO.toEntity(): ProductRegistration = ProductRegistration(
+    id = id, supplierId = supplierId, supplierRef = supplierRef, hmsArtNr = hmsArtNr, title = title,
+    articleName = articleName, draftStatus = draftStatus, adminStatus = adminStatus,
+    registrationStatus = registrationStatus, message = message, adminInfo = adminInfo, created = created,
     updated = updated, published = published, expired = expired, updatedByUser = updatedByUser,
     createdByUser = createdByUser, createdBy = createdBy, updatedBy = updatedBy, createdByAdmin = createdByAdmin,
-    productDTO = productDTO, version = version
+    productData = productData
 )
 
  fun ProductRegistration.toDTO(): ProductRegistrationDTO = ProductRegistrationDTO(
-     id = id, draftStatus = draftStatus, adminStatus = adminStatus, registrationStatus = registrationStatus,
-     message = message, adminInfo = adminInfo, created = created, updated = updated, published = published,
-     expired = expired, updatedByUser = updatedByUser, createdByUser = createdByUser, createdBy = createdBy,
-     updatedBy = updatedBy, createdByAdmin = createdByAdmin, productDTO = productDTO, version = version
+     id = id, supplierId = supplierId, supplierRef = supplierRef, hmsArtNr = hmsArtNr, title = title,
+     articleName = articleName, draftStatus = draftStatus, adminStatus = adminStatus,
+     registrationStatus = registrationStatus, message = message, adminInfo = adminInfo, created = created,
+     updated = updated, published = published, expired = expired, updatedByUser = updatedByUser,
+     createdByUser = createdByUser, createdBy = createdBy, updatedBy = updatedBy, createdByAdmin = createdByAdmin,
+     productData = productData
 )
