74b4763e3bc6489bc4e575229cf88e5df1a1cc00
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-04-03 08:21:27 +0200

    separate product domain, introduce productdata

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val tcVersion= "1.17.6"
 val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val rapidsRiversVersion="202303011052"
-val grunndataDtoVersion = "202303301323"
+val grunndataDtoVersion = "202304030755"
 
 group = "no.nav.hm"
 version = properties["version"] ?: "local-build"
--- src/main/kotlin/no/nav/hm/grunndata/register/product/ProductData.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductData.kt
@@ -1,67 +1,28 @@
 package no.nav.hm.grunndata.register.product
 
-import no.nav.hm.grunndata.rapid.dto.MediaSourceType
-import java.time.LocalDateTime
-import java.util.*
+import no.nav.hm.grunndata.rapid.dto.*
 
 
 data class ProductData (
-    val title: String,
-    val articleName: String,
     val attributes: Attributes = Attributes(),
-    val hmsArtNr: String?=null,
-    val supplierRef: String,
     val isoCategory: String,
     val accessory: Boolean = false,
     val sparePart: Boolean = false,
     val seriesId: String?,
-    val transferTechData: List<ProductTechData> = emptyList(),
-    val media: List<ProductMedia> = emptyList(),
-    val published: LocalDateTime = LocalDateTime.now(),
-    val expired: LocalDateTime = published.plusYears(10),
+    val techData: List<TechData> = emptyList(),
+    val media: List<MediaInfo> = emptyList(),
     val agreementInfo: AgreementInfo?=null,
 )
 
-data class ProductMedia (
-    val sourceUri: String,
-    val uri: String = sourceUri,
-    val priority: Int = 1,
-    val type: MediaType = MediaType.IMAGE,
-    val text:   String?=null,
-    val sourceType: MediaSourceType = MediaSourceType.EXTERNALURL
+
+
+fun ProductDTO.toProductData(): ProductData = ProductData (
+    isoCategory = isoCategory,
+    accessory = accessory,
+    sparePart = sparePart,
+    seriesId = seriesId,
+    techData = techData,
+    media = media,
+    agreementInfo = agreementInfo,
+    attributes = attributes
 )
-
-enum class TransferProductStatus {
-    ACTIVE, INACTIVE
-}
-
-data class ProductTechData (
-    val key:    String,
-    val value:  String,
-    val unit:   String
-)
-
-data class AgreementInfo (
-    val rank: Int,
-    val postNr: Int,
-    val reference: String
-)
-
-enum class MediaType {
-    PDF,
-    IMAGE,
-    VIDEO
-}
-
-data class Attributes(val manufacturer: String? = null,
-                      val compatible: List<CompatibleAttribute>? = null,
-                      val series: String? = null,
-                      val shortdescription: String? = null,
-                      val text: String? = null,
-                      val url: String? = null)
-
-
-data class CompatibleAttribute(val id: UUID?=null,
-                               val supplierRef: String?=null,
-                               val hmsArtNr: String?)
-
