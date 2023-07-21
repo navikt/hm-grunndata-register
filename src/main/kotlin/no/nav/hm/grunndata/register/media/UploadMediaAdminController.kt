caecc7767e35c283ccfe46585cc1783226e046ec
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-07-21 13:57:00 +0200

    upload media admin controller fix

--- src/main/kotlin/no/nav/hm/grunndata/register/media/UploadMediaAdminController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/media/UploadMediaAdminController.kt
@@ -12,40 +12,56 @@ import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.reactive.asFlow
 import no.nav.hm.grunndata.rapid.dto.MediaDTO
 import no.nav.hm.grunndata.rapid.dto.MediaType
-import no.nav.hm.grunndata.register.media.UploadMediaController.Companion.V1_UPLOAD_MEDIA
+import no.nav.hm.grunndata.register.api.BadRequestException
+import no.nav.hm.grunndata.register.media.UploadMediaAdminController.Companion.API_V1_ADMIN_UPLOAD_MEDIA
+import no.nav.hm.grunndata.register.product.ProductRegistrationService
 import no.nav.hm.grunndata.register.security.Roles
 import org.reactivestreams.Publisher
 import org.slf4j.LoggerFactory
 import java.util.*
 
-@Secured(Roles.ROLE_ADMIN, Roles.ROLE_SUPPLIER)
-@Controller(V1_UPLOAD_MEDIA)
-class UploadMediaController(private val mediaUploadClient: MediaUploadClient) {
+@Secured(Roles.ROLE_ADMIN)
+@Controller(API_V1_ADMIN_UPLOAD_MEDIA)
+class UploadMediaAdminController(private val mediaUploadClient: MediaUploadClient,
+                                 private val productRegistrationService: ProductRegistrationService,
+                                 private val agreementRegistrationService: ProductRegistrationService) {
 
     companion object {
-        const val V1_UPLOAD_MEDIA = "/api/v1/admin/media"
-        private val LOG = LoggerFactory.getLogger(UploadMediaController::class.java)
+        const val API_V1_ADMIN_UPLOAD_MEDIA = "/api/v1/admin/media"
+        private val LOG = LoggerFactory.getLogger(UploadMediaAdminController::class.java)
     }
 
     @Post(
-        value = "/file/{oid}",
+        value = "/{type}/file/{oid}",
         consumes = [io.micronaut.http.MediaType.MULTIPART_FORM_DATA],
         produces = [io.micronaut.http.MediaType.APPLICATION_JSON]
     )
     suspend fun uploadFile(oid: UUID,
+                           type: String,
                            file: CompletedFileUpload,
-                           authentication: Authentication): HttpResponse<MediaDTO> =
-        HttpResponse.created(createMedia(file, oid))
+                           authentication: Authentication): HttpResponse<MediaDTO> {
+        if ("product"==type && productRegistrationService.findById(oid) != null
+            || "agreement"==type && agreementRegistrationService.findById(oid)!=null) {
+            return HttpResponse.created(createMedia(file, oid))
+        }
+        throw BadRequestException("Unknown oid, must be of product or agreement")
+    }
 
     @Post(
-        value = "/files/{oid}",
+        value = "/{type}/files/{oid}",
         consumes = [io.micronaut.http.MediaType.MULTIPART_FORM_DATA],
         produces = [io.micronaut.http.MediaType.APPLICATION_JSON]
     )
     suspend fun uploadFiles(oid: UUID,
+                            type: String,
                             files: Publisher<CompletedFileUpload>,
-                            authentication: Authentication): HttpResponse<List<MediaDTO>> =
-            HttpResponse.created(files.asFlow().map {createMedia(it, oid) }.toList())
+                            authentication: Authentication): HttpResponse<List<MediaDTO>>  {
+        if ("product"==type && productRegistrationService.findById(oid) != null
+            || "agreement"==type && agreementRegistrationService.findById(oid)!=null) {
+            return HttpResponse.created(files.asFlow().map {createMedia(it, oid) }.toList())
+        }
+        throw BadRequestException("Unknown oid, must be of product or agreement")
+    }
 
 
 
