43289033d5f19703148bbce89f81866a81a3182b
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-08-14 12:54:27 +0200

    added controller to fech labels

---
+++ src/main/kotlin/no/nav/hm/grunndata/register/techlabel/TechLabelController.kt
@@ -0,0 +1,17 @@
+package no.nav.hm.grunndata.register.techlabel
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.openapi.visitor.security.SecurityRule
+import io.micronaut.security.annotation.Secured
+
+@Secured(SecurityRule.IS_ANONYMOUS)
+@Controller("/api/v1/techlabels")
+class TechLabelController(private val techLabelService: TechLabelService) {
+
+    @Get("/")
+    fun fetchTechLabels() = techLabelService.fetchAllLabels()
+
+    @Get("/{isocode}")
+    fun fetchTechLabelsByIsocode(isocode: String) = techLabelService.fetchLabelsByIsoCode(isocode)
+}
