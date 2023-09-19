2279644d6b5c2a49a372302cb9a6dfb1c7680959
marte pedersen <marte.pedersen@nav.no>
2023-09-19 15:07:21 +0200

    fjerner nullsjekk på authentication alt som kommer inn er autentisert fra laget over formattering

--- src/main/kotlin/no/nav/hm/grunndata/register/user/UserController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/user/UserController.kt
@@ -4,10 +4,6 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.*
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
-import io.swagger.v3.oas.annotations.security.SecurityScheme
 import no.nav.hm.grunndata.register.api.BadRequestException
 import no.nav.hm.grunndata.register.security.Roles
 import org.slf4j.LoggerFactory
@@ -23,45 +19,46 @@ class UserController(private val userRepository: UserRepository) {
         private val LOG = LoggerFactory.getLogger(UserController::class.java)
         const val API_V1_USER_REGISTRATIONS = "/vendor/api/v1/users"
     }
+
     @Get("/")
-    suspend fun getUsers(authentication: Authentication?): HttpResponse<List<UserDTO>> =
-        if (authentication!=null) {
-            userRepository.getUsersBySupplierId(authentication.attributes[SUPPLIER_ID].toString())
-                .map { it.toDTO() }
-                .let {
-                    HttpResponse.ok(it)
-                } ?: HttpResponse.notFound()
-        }
-        else HttpResponse.unauthorized()
+    suspend fun getUsers(authentication: Authentication): HttpResponse<List<UserDTO>> =
+        userRepository.getUsersBySupplierId(authentication.attributes[SUPPLIER_ID].toString())
+            .map { it.toDTO() }
+            .let {
+                HttpResponse.ok(it)
+            } ?: HttpResponse.notFound()
 
     @Get("/{userId}")
-    suspend fun getUserId(userId: UUID, authentication: Authentication?): HttpResponse<UserDTO> =
-        if (authentication != null) {
-            userRepository.findById(userId)
-                ?.takeIf { it.attributes[SUPPLIER_ID] == authentication.attributes[SUPPLIER_ID] }
-                ?.let {
-                    HttpResponse.ok(it.toDTO())
-                } ?: HttpResponse.notFound()
-        } else HttpResponse.unauthorized()
+    suspend fun getUserId(userId: UUID, authentication: Authentication): HttpResponse<UserDTO> =
+        userRepository.findById(userId)
+            ?.takeIf { it.attributes[SUPPLIER_ID] == authentication.attributes[SUPPLIER_ID] }
+            ?.let {
+                HttpResponse.ok(it.toDTO())
+            } ?: HttpResponse.notFound()
 
     @Put("/")
-    suspend fun updateUser(authentication: Authentication?, @Body userDTO: UserDTO): HttpResponse<UserDTO> =
-        if (authentication != null) {
-            userRepository.findByEmail(authentication.name)
-                ?.let { HttpResponse.ok(userRepository.update(it.copy(name = userDTO.name, email = userDTO.email,
-                    roles = userDTO.roles, attributes = userDTO.attributes)).toDTO()) } ?: HttpResponse.notFound()
-        }
-        else HttpResponse.unauthorized()
+    suspend fun updateUser(authentication: Authentication, @Body userDTO: UserDTO): HttpResponse<UserDTO> =
+        userRepository.findByEmail(authentication.name)
+            ?.let {
+                HttpResponse.ok(
+                    userRepository.update(
+                        it.copy(
+                            name = userDTO.name, email = userDTO.email,
+                            roles = userDTO.roles, attributes = userDTO.attributes
+                        )
+                    ).toDTO()
+                )
+            } ?: HttpResponse.notFound()
 
     @Put("/password")
-    suspend fun changePassword(authentication: Authentication?, @Body changePassword: ChangePasswordDTO): HttpResponse<Any> =
-        if (authentication != null) {
-            userRepository.loginUser(authentication.name, changePassword.oldPassword)?.let {
-                userRepository.changePassword(it.id, changePassword.oldPassword, changePassword.newPassword)
-                HttpResponse.ok()
-            } ?: throw BadRequestException("Wrong user info, please check password and email is correct")
-        }
-        else HttpResponse.unauthorized()
+    suspend fun changePassword(
+        authentication: Authentication,
+        @Body changePassword: ChangePasswordDTO
+    ): HttpResponse<Any> =
+        userRepository.loginUser(authentication.name, changePassword.oldPassword)?.let {
+            userRepository.changePassword(it.id, changePassword.oldPassword, changePassword.newPassword)
+            HttpResponse.ok()
+        } ?: throw BadRequestException("Wrong user info, please check password and email is correct")
 }
 
 data class ChangePasswordDTO(val oldPassword: String, val newPassword: String)
