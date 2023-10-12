c4cf49f9f10ed7751432d6cbee66ff5c03a1e03a
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-10-12 08:20:13 +0200

    cache not working with suspend

--- src/main/kotlin/no/nav/hm/grunndata/register/supplier/SupplierRegistrationService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/supplier/SupplierRegistrationService.kt
@@ -1,32 +1,23 @@
 package no.nav.hm.grunndata.register.supplier
 
-import io.micronaut.cache.annotation.CacheConfig
-import io.micronaut.cache.annotation.CacheInvalidate
-import io.micronaut.cache.annotation.Cacheable
 import io.micronaut.data.model.Page
 import io.micronaut.data.model.Pageable
 import io.micronaut.data.repository.jpa.criteria.PredicateSpecification
 import jakarta.inject.Singleton
 import jakarta.transaction.Transactional
-import kotlinx.coroutines.flow.map
-import no.nav.hm.grunndata.register.product.toDTO
 import java.util.*
 
 @Singleton
-@CacheConfig("suppliers")
 open class SupplierRegistrationService(private val supplierRepository: SupplierRepository,
                                        private val supplierRegistrationHandler: SupplierRegistrationHandler) {
 
-    @Cacheable
     open suspend fun findById(id: UUID): SupplierRegistrationDTO? = supplierRepository.findById(id)?.toDTO()
 
     open suspend fun findByName(name: String): SupplierRegistrationDTO?= supplierRepository.findByName(name)?.toDTO()
 
-    @CacheInvalidate(parameters = ["id"])
     open suspend fun update(dto: SupplierRegistrationDTO, id: UUID = dto.id) =
         supplierRepository.update(dto.toEntity()).toDTO()
 
-    @CacheInvalidate(parameters = ["id"])
     open suspend fun save(dto: SupplierRegistrationDTO, id: UUID = dto.id) =
         supplierRepository.save(dto.toEntity()).toDTO()
 
