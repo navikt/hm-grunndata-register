beae3d593ad088c824fd9f404ca2d28471636ed8
marte pedersen <marte.pedersen@nav.no>
2023-10-23 13:06:00 +0200

    wip på seriehenting

---
+++ src/main/kotlin/no/nav/hm/grunndata/register/series/SeriesController.kt
@@ -0,0 +1,46 @@
+package no.nav.hm.grunndata.register.series
+
+import io.micronaut.data.model.Page
+import io.micronaut.data.model.Pageable
+import io.micronaut.data.repository.jpa.criteria.PredicateSpecification
+import io.micronaut.data.runtime.criteria.get
+import io.micronaut.data.runtime.criteria.where
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.authentication.Authentication
+import no.nav.hm.grunndata.register.product.ProductRegistration
+import no.nav.hm.grunndata.register.security.Roles
+import org.slf4j.LoggerFactory
+
+@Secured(Roles.ROLE_SUPPLIER)
+@Controller(SeriesController.API_V1_SERIES)
+class SeriesController(private val seriesRegistrationService: SeriesRegistrationService) {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(SeriesController::class.java)
+        const val API_V1_SERIES = "/vendor/api/v1/series"
+    }
+
+    @Get("/")
+    suspend fun getSeries(
+        @QueryValue params: HashMap<String, String>?,
+        pageable: Pageable,
+        authentication: Authentication
+    ): Page<SeriesRegistrationDTO> =
+        seriesRegistrationService.findAll(buildCriteriaSpec(params), pageable)
+
+    private fun buildCriteriaSpec(params: HashMap<String, String>?): PredicateSpecification<SeriesRegistration>? =
+        params?.let {
+            where {
+                if (params.contains("supplierId")) root[SeriesRegistration::supplierId] eq params["supplierId"]
+                if (params.contains("identifier")) root[SeriesRegistration::identifier] eq params["identifier"]
+            }.and { root, criteriaBuilder ->
+                if (params.contains("title")) criteriaBuilder.like(
+                    root[SeriesRegistration::title],
+                    params["title"]
+                ) else null
+            }
+        }
+}
---
+++ src/test/kotlin/no/nav/hm/grunndata/register/series/SeriesControllerTest.kt
@@ -0,0 +1,116 @@
+package no.nav.hm.grunndata.register.series
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import io.micronaut.data.model.Page
+import io.micronaut.data.model.Pageable
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.MediaType
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.security.authentication.UsernamePasswordCredentials
+import io.micronaut.test.annotation.MockBean
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.mockk.mockk
+import jakarta.inject.Inject
+import java.util.UUID
+import kotlinx.coroutines.runBlocking
+import no.nav.hm.grunndata.register.CONTEXT_PATH
+import no.nav.hm.grunndata.register.security.LoginClient
+import no.nav.hm.grunndata.register.security.Roles
+import no.nav.hm.grunndata.register.supplier.SupplierData
+import no.nav.hm.grunndata.register.supplier.SupplierRegistrationDTO
+import no.nav.hm.grunndata.register.supplier.SupplierRegistrationService
+import no.nav.hm.grunndata.register.user.User
+import no.nav.hm.grunndata.register.user.UserAttribute
+import no.nav.hm.grunndata.register.user.UserControllerTest
+import no.nav.hm.grunndata.register.user.UserRepository
+import no.nav.hm.rapids_rivers.micronaut.RapidPushService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+
+@MicronautTest
+class SeriesControllerTest(
+    private val seriesRegistrationService: SeriesRegistrationService,
+    private val loginClient: LoginClient,
+    private val userRepository: UserRepository,
+    private val supplierRegistrationService: SupplierRegistrationService,
+    private val objectMapper: ObjectMapper
+) {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(UserControllerTest::class.java)
+    }
+
+    @Inject
+    @field:Client("$CONTEXT_PATH/")
+    lateinit var client: HttpClient
+
+    val email = "user@test.test"
+    val token = "token-123"
+
+    @MockBean(RapidPushService::class)
+    fun rapidPushService(): RapidPushService = mockk(relaxed = true)
+
+    @BeforeEach
+    fun createUserSupplier() {
+        runBlocking {
+            val testSupplierRegistration = supplierRegistrationService.save(
+                SupplierRegistrationDTO(
+                    id = UUID.randomUUID(),
+                    supplierData = SupplierData(
+                        email = "supplier@test.test",
+                        address = "address 1",
+                        homepage = "https://www.hompage.no",
+                        phone = "+47 12345678"
+                    ),
+                    identifier = "supplier-unique-name",
+                    name = "Supplier AS"
+                )
+            )
+            val user = User(
+                email = email, token = token,
+                name = "User tester", roles = listOf(Roles.ROLE_SUPPLIER),
+                attributes = mapOf(Pair(UserAttribute.SUPPLIER_ID, testSupplierRegistration.id.toString()))
+            )
+            userRepository.createUser(user)
+
+            val serie = seriesRegistrationService.save(
+                SeriesRegistrationDTO(
+                    id = UUID.randomUUID(),
+                    supplierId = testSupplierRegistration.id,
+                    identifier = "testerino",
+                    title = "superserie"
+                )
+            )
+
+            LOG.info("created supplier: ${objectMapper.writeValueAsString(testSupplierRegistration)}")
+            LOG.info("created user: ${objectMapper.writeValueAsString(user)}")
+            LOG.info("created series: ${objectMapper.writeValueAsString(serie)}")
+        }
+    }
+
+
+    @Test
+    fun testert() {
+        val jwt = loginClient.login(UsernamePasswordCredentials(email, token)).getCookie("JWT").get()
+
+        val uri = "${SeriesController.API_V1_SERIES}/?title=superserie"
+
+        val response = client.toBlocking().exchange(
+            HttpRequest.GET<Page<SeriesRegistrationDTO>>(uri)
+                .accept(MediaType.APPLICATION_JSON)
+                .cookie(jwt),
+            Page::class.java
+        )
+            .shouldNotBeNull()
+            .body().shouldNotBeNull()
+
+        response.totalSize shouldBe 1
+
+        println(objectMapper.writeValueAsString(response))
+    }
+
+}
