b5126982771e13b7557dd6d2d4785b0269eac97f
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-12-01 08:44:28 +0100

    give an error message when drafting variant with used supplierRef

--- src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationApiController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationApiController.kt
@@ -127,9 +127,14 @@ class ProductRegistrationApiController(private val productRegistrationService: P
 
     @Post("/draft/variant/{id}")
     suspend fun createProductVariant(@PathVariable id:UUID, @Body draftVariant: DraftVariantDTO, authentication: Authentication): HttpResponse<ProductRegistrationDTO> {
-        return productRegistrationService.createProductVariant(id, draftVariant, authentication)?.let {
-            HttpResponse.ok(it)
-        } ?: HttpResponse.notFound()
+        return try {
+            productRegistrationService.createProductVariant(id, draftVariant, authentication)?.let {
+                HttpResponse.ok(it)
+            } ?: HttpResponse.notFound()
+        } catch (e: Exception) {
+            LOG.error("Got exception while creating variant ${draftVariant.supplierRef}", e)
+            throw BadRequestException("Could not create variant for ${draftVariant.supplierRef}, already exists")
+        }
     }
 }
 
