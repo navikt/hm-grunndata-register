a219a05442fee85f1d0d75bfa631dc86175205ea
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-01-15 09:26:21 +0100

    read bestllingsordning json file from github

---
+++ src/main/kotlin/no/nav/hm/grunndata/register/bestillingsordning/Bestillingsordning.kt
@@ -0,0 +1,29 @@
+package no.nav.hm.grunndata.register.bestillingsordning
+
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.micronaut.context.annotation.Value
+import jakarta.inject.Singleton
+import java.net.URL
+
+@Singleton
+class Bestillingsordning(
+    @Value("\${bestillingsordning.url}")
+    private val url : String,
+    private val objectMapper: ObjectMapper) {
+
+    private val boMap: Map<String, BestillingsordningDTO> =
+        objectMapper.readValue(URL(url), object : TypeReference<List<BestillingsordningDTO>>(){}).associateBy { it.hmsnr }
+
+    fun isBestillingsordning(hmsnr: String): Boolean = boMap.containsKey(hmsnr)
+
+    fun getBestillingsorning(hmsnr: String): BestillingsordningDTO? = boMap[hmsnr]
+
+}
+
+data class BestillingsordningDTO(
+    val hmsnr: String,
+    val navn: String
+)
+
+
