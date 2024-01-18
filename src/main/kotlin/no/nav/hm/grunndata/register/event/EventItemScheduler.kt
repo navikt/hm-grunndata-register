3ae1020e667037a22b3d8ae5aae271048f5a73d3
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-01-18 09:02:41 +0100

    collect all eventhandlers in eventItemService

--- src/main/kotlin/no/nav/hm/grunndata/register/event/EventItemScheduler.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/event/EventItemScheduler.kt
@@ -5,12 +5,6 @@ import io.micronaut.scheduling.annotation.Scheduled
 import jakarta.inject.Singleton
 import kotlinx.coroutines.runBlocking
 import no.nav.hm.grunndata.register.LeaderElection
-import no.nav.hm.grunndata.register.agreement.AgreementRegistrationEventHandler
-import no.nav.hm.grunndata.register.bestillingsordning.BestillingsordningEventHandler
-import no.nav.hm.grunndata.register.product.ProductRegistrationEventHandler
-import no.nav.hm.grunndata.register.productagreement.ProductAgreementRegistrationEventHandler
-import no.nav.hm.grunndata.register.series.SeriesRegistrationEventHandler
-import no.nav.hm.grunndata.register.supplier.SupplierRegistrationHandler
 import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
 
@@ -18,13 +12,7 @@ import java.time.LocalDateTime
 @Requires(property = "schedulers.enabled", value = "true")
 class EventItemScheduler(
     private val eventItemService: EventItemService,
-    private val leaderElection: LeaderElection,
-    private val agreementRegistrationEventHandler: AgreementRegistrationEventHandler,
-    private val productRegistrationEventHandler: ProductRegistrationEventHandler,
-    private val seriesRegistrationEventHandler: SeriesRegistrationEventHandler,
-    private val supplierRegistrationHandler: SupplierRegistrationHandler,
-    private val productAgreementRegistrationHandler: ProductAgreementRegistrationEventHandler,
-    private val bestillingsordningEventHandler: BestillingsordningEventHandler
+    private val leaderElection: LeaderElection
 ) {
 
     companion object {
@@ -39,15 +27,7 @@ class EventItemScheduler(
                 LOG.info("Running sendEventItemScheduler with ${items.size} items")
                 items.forEach {
                     LOG.info("sending event ${it.oid} with type ${it.type}")
-                    when (it.type) {
-                        EventItemType.AGREEMENT -> agreementRegistrationEventHandler.sendRapidEvent(it)
-                        EventItemType.PRODUCT -> productRegistrationEventHandler.sendRapidEvent(it)
-                        EventItemType.SERIES -> seriesRegistrationEventHandler.sendRapidEvent(it)
-                        EventItemType.SUPPLIER -> supplierRegistrationHandler.sendRapidEvent(it)
-                        EventItemType.PRODUCTAGREEMENT -> productAgreementRegistrationHandler.sendRapidEvent(it)
-                        EventItemType.BESTILLINGSORDNING -> bestillingsordningEventHandler.sendRapidEvent(it)
-                    }
-                    eventItemService.setEventItemStatusToSent(it)
+                    eventItemService.sendRapidEvent(it)
                 }
             }
         }
