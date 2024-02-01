e0c8cd9e33211b6ffd87ac829c326682c5f150c3
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-02-01 13:25:20 +0100

    add seriesIdentifier aswell

--- src/main/kotlin/no/nav/hm/grunndata/register/product/ProductData.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductData.kt
@@ -1,10 +1,14 @@
 package no.nav.hm.grunndata.register.product
 
-import no.nav.hm.grunndata.rapid.dto.*
+import no.nav.hm.grunndata.rapid.dto.Attributes
+import no.nav.hm.grunndata.rapid.dto.MediaInfo
+import no.nav.hm.grunndata.rapid.dto.ProductRapidDTO
+import no.nav.hm.grunndata.rapid.dto.TechData
 
 
 data class ProductData (
     val identifier: String? = null,
+    val seriesIdentifier: String?= null,
     val attributes: Attributes = Attributes(),
     val accessory: Boolean = false,
     val sparePart: Boolean = false,
@@ -20,5 +24,6 @@ fun ProductRapidDTO.toProductData(): ProductData = ProductData (
     techData = techData,
     media = media,
     identifier = identifier,
+    seriesIdentifier = seriesIdentifier,
     attributes = attributes
 )
