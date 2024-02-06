1a80e0e3656035aaeb67733c46bd431a39c91e41
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-02-06 12:12:14 +0100

    added sort variable to techlabels

--- src/test/kotlin/no/nav/hm/grunndata/register/techlabel/TechLabelRegistrationRepositoryTest.kt
+++ src/test/kotlin/no/nav/hm/grunndata/register/techlabel/TechLabelRegistrationRepositoryTest.kt
@@ -16,7 +16,7 @@ class TechLabelRegistrationRepositoryTest(private val techLabelRegistrationRepos
     @Test
     fun crudTest() {
         val techLabel = TechLabelRegistration(id = UUID.randomUUID(), identifier = "HMDB-20815", label = "Høyde", guide="Høyde",
-            isoCode = "09070601", type = "N", unit = "cm")
+            isoCode = "09070601", type = "N", unit = "cm", sort = 1)
         runBlocking {
             val saved = techLabelRegistrationRepository.save(techLabel)
             val found = techLabelRegistrationRepository.findById(saved.id)
@@ -27,6 +27,7 @@ class TechLabelRegistrationRepositoryTest(private val techLabelRegistrationRepos
             found.isoCode shouldBe "09070601"
             found.type shouldBe "N"
             found.unit shouldBe "cm"
+            found.sort shouldBe 1
             val updated = techLabelRegistrationRepository.update(found.copy(guide = "Høyde eller noe", updated = LocalDateTime.now()))
             updated.shouldNotBeNull()
             updated.updated shouldBeAfter saved.updated
