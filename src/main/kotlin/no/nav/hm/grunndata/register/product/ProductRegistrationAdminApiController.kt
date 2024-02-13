8af73f10f32be4a9a4804a608314b5d586165c7f
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-02-13 11:54:12 +0100

    swagger does not support get with body

--- src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationAdminApiController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationAdminApiController.kt
@@ -166,7 +166,7 @@ class ProductRegistrationAdminApiController(private val productRegistrationServi
             HttpResponse.ok(dto)
         }?: HttpResponse.notFound()
 
-    @Get("/excel/export", consumes = ["application/json"], produces = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"])
+    @Post("/excel/export", consumes = ["application/json"], produces = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"])
     suspend fun createExport(@Body uuids: List<UUID>, authentication: Authentication): HttpResponse<ByteArrayOutputStream> {
         val products = uuids.map { productRegistrationService.findById(it)}.filterNotNull()
         if (products.isEmpty()) throw BadRequestException("No products found")
