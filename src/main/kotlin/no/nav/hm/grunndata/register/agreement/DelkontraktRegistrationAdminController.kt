9828ecf8c517434f920d08e5df7e3e3abea99f88
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-02-28 18:14:01 +0100

    Sorter delkontrakter på sortNr

--- src/main/kotlin/no/nav/hm/grunndata/register/agreement/DelkontraktRegistrationAdminController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/agreement/DelkontraktRegistrationAdminController.kt
@@ -1,7 +1,12 @@
 package no.nav.hm.grunndata.register.agreement
 
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.annotation.*
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Put
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
 import no.nav.hm.grunndata.rapid.dto.DraftStatus
@@ -9,22 +14,24 @@ import no.nav.hm.grunndata.register.error.BadRequestException
 import no.nav.hm.grunndata.register.security.Roles
 import no.nav.hm.grunndata.register.security.userId
 import org.slf4j.LoggerFactory
-import java.util.*
+import java.util.UUID
 
 @Secured(Roles.ROLE_ADMIN)
 @Controller(DelkontraktRegistrationAdminController.API_V1_ADMIN_DELKONTRAKT_REGISTRATIONS)
-class DelkontraktRegistrationAdminController(private val delkontraktRegistrationService: DelkontraktRegistrationService,
-                                             private val agreementService: AgreementRegistrationService) {
-
+class DelkontraktRegistrationAdminController(
+    private val delkontraktRegistrationService: DelkontraktRegistrationService,
+    private val agreementService: AgreementRegistrationService,
+) {
     companion object {
         const val API_V1_ADMIN_DELKONTRAKT_REGISTRATIONS = "/admin/api/v1/agreement/delkontrakt/registrations"
         private val LOG = LoggerFactory.getLogger(DelkontraktRegistrationAdminController::class.java)
     }
 
     @Get("/agreement/{agreementId}")
-    suspend fun findByAgreementId(agreementId: UUID): List<DelkontraktRegistrationDTO> =
-        delkontraktRegistrationService.findByAgreementId(agreementId)
-
+    suspend fun findByAgreementId(agreementId: UUID): List<DelkontraktRegistrationDTO> {
+        val delkontrakter = delkontraktRegistrationService.findByAgreementId(agreementId)
+        return delkontrakter.sortedBy { it.delkontraktData.sortNr }
+    }
 
     @Get("/{id}")
     suspend fun findByDelkontraktId(id: UUID): HttpResponse<DelkontraktRegistrationDTO?> =
@@ -33,7 +40,7 @@ class DelkontraktRegistrationAdminController(private val delkontraktRegistration
     @Post("/")
     suspend fun createDelkontrakt(
         @Body dto: DelkontraktRegistrationDTO,
-        authentication: Authentication
+        authentication: Authentication,
     ): HttpResponse<DelkontraktRegistrationDTO> =
         delkontraktRegistrationService.findById(dto.id)?.let {
             throw BadRequestException("Delkontrakt ${dto.id} already exists")
@@ -43,25 +50,26 @@ class DelkontraktRegistrationAdminController(private val delkontraktRegistration
     suspend fun updateDelkontrakt(
         id: UUID,
         @Body dto: DelkontraktRegistrationDTO,
-        authentication: Authentication
+        authentication: Authentication,
     ): HttpResponse<DelkontraktRegistrationDTO> =
         delkontraktRegistrationService.findById(id)?.let {
             HttpResponse.ok(delkontraktRegistrationService.update(dto))
-        } ?: HttpResponse.notFound(
-    )
+        } ?: HttpResponse.notFound()
 
     @Delete("/{id}")
-    suspend fun deleteDelkontraktById(id: UUID, authentication: Authentication): HttpResponse<Unit> {
+    suspend fun deleteDelkontraktById(
+        id: UUID,
+        authentication: Authentication,
+    ): HttpResponse<Unit> {
         LOG.info("deleting delkontrakt $id by user ${authentication.userId()}")
         return delkontraktRegistrationService.findById(id)?.let { inDb ->
-                val agreement = agreementService.findById(inDb.agreementId)
-                if (agreement!=null && agreement.draftStatus == DraftStatus.DRAFT) {
-                    delkontraktRegistrationService.deleteById(id)
-                    HttpResponse.noContent()
-                } else {
-                    throw BadRequestException("Delkontrakt $id cannot be deleted")
-                }
+            val agreement = agreementService.findById(inDb.agreementId)
+            if (agreement != null && agreement.draftStatus == DraftStatus.DRAFT) {
+                delkontraktRegistrationService.deleteById(id)
+                HttpResponse.noContent()
+            } else {
+                throw BadRequestException("Delkontrakt $id cannot be deleted")
+            }
         } ?: HttpResponse.notFound()
     }
-
 }
