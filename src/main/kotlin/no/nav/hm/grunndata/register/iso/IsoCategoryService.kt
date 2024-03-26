a26c278cf0065c542c469a46831e9f2d925af0e9
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-03-26 08:29:28 +0100

    fetch isocategories from hm-grunndata-db and populate database

--- src/main/kotlin/no/nav/hm/grunndata/register/iso/IsoCategoryService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/iso/IsoCategoryService.kt
@@ -1,13 +1,19 @@
 package no.nav.hm.grunndata.register.iso
 
 import jakarta.inject.Singleton
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import no.nav.hm.grunndata.rapid.dto.IsoCategoryDTO
+import no.nav.hm.grunndata.register.HMDB
 import no.nav.hm.grunndata.register.gdb.GdbApiClient
 import org.slf4j.LoggerFactory
 
 @Singleton
-class IsoCategoryService(gdbApiClient: GdbApiClient) {
+class IsoCategoryService(
+    private val gdbApiClient: GdbApiClient,
+    private val isoCategoryRegistrationRepository: IsoCategoryRegistrationRepository
+) {
 
     private var isoCategories: Map<String, IsoCategoryDTO>
 
@@ -18,7 +24,34 @@ class IsoCategoryService(gdbApiClient: GdbApiClient) {
 
     init {
         runBlocking {
-            isoCategories =  gdbApiClient.retrieveIsoCategories().associateBy { it.isoCode }
+            isoCategories =
+                isoCategoryRegistrationRepository.findAll().map { it.toRapidDTO() }.toList().associateBy { it.isoCode }
+            if (isoCategories.isEmpty()) {
+                val categories = gdbApiClient.retrieveIsoCategories()
+                isoCategories = categories.associateBy { it.isoCode }
+                isoCategories.values.forEach {
+                    isoCategoryRegistrationRepository.save(
+                        IsoCategoryRegistration(
+                            isoCode = it.isoCode,
+                            isoTitle = it.isoTitle,
+                            isoText = it.isoText,
+                            isoTranslations = IsoTranslations(
+                                titleEn = it.isoTranslations?.titleEn,
+                                textEn = it.isoTranslations?.textEn
+                            ),
+                            isoTextShort = it.isoTextShort ?: "",
+                            isoLevel = it.isoLevel,
+                            isActive = it.isActive,
+                            showTech = it.showTech,
+                            allowMulti = it.allowMulti,
+                            createdByUser = HMDB,
+                            updatedByUser = HMDB,
+                            createdBy = HMDB,
+                            updatedBy = HMDB
+                        )
+                    )
+                }
+            }
         }
         LOG.info("Got isoCategories: ${isoCategories.size}")
     }
