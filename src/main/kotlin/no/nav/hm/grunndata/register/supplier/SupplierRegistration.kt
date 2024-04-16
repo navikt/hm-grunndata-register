9a513eb2486ba7ded583a69c067e7300551dd5d5
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-04-16 14:08:37 +0200

    Supplier starts with draft = DONE

--- src/main/kotlin/no/nav/hm/grunndata/register/supplier/SupplierRegistration.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/supplier/SupplierRegistration.kt
@@ -11,16 +11,16 @@ import no.nav.hm.grunndata.register.event.EventPayload
 import java.time.LocalDateTime
 import java.util.*
 
-const val SUPPLIER_V1= "supplier_reg_v1"
+const val SUPPLIER_V1 = "supplier_reg_v1"
 
 @MappedEntity(SUPPLIER_V1)
-data class SupplierRegistration (
+data class SupplierRegistration(
     @field:Id
     val id: UUID,
     val status: SupplierStatus = SupplierStatus.ACTIVE,
-    val draftStatus: DraftStatus = DraftStatus.DRAFT,
+    val draftStatus: DraftStatus = DraftStatus.DONE,
     val name: String,
-    @field:TypeDef(type= DataType.JSON)
+    @field:TypeDef(type = DataType.JSON)
     val supplierData: SupplierData,
     val identifier: String,
     val created: LocalDateTime = LocalDateTime.now(),
@@ -28,23 +28,23 @@ data class SupplierRegistration (
     val createdBy: String = RapidApp.grunndata_register,
     val updatedBy: String = RapidApp.grunndata_register,
     val updatedByUser: String = "admin",
-    val createdByUser: String = "admin"
+    val createdByUser: String = "admin",
 )
 
 data class SupplierData(
-    val address: String?=null,
-    val postNr: String?=null,
-    val postLocation: String?=null,
-    val countryCode: String?=null,
-    val email: String?=null,
-    val phone: String?=null,
-    val homepage: String?=null
+    val address: String? = null,
+    val postNr: String? = null,
+    val postLocation: String? = null,
+    val countryCode: String? = null,
+    val email: String? = null,
+    val phone: String? = null,
+    val homepage: String? = null,
 )
 
-data class SupplierRegistrationDTO (
+data class SupplierRegistrationDTO(
     override val id: UUID = UUID.randomUUID(),
     val status: SupplierStatus = SupplierStatus.ACTIVE,
-    val draftStatus: DraftStatus = DraftStatus.DRAFT,
+    val draftStatus: DraftStatus = DraftStatus.DONE,
     val name: String,
     val supplierData: SupplierData,
     val identifier: String = UUID.randomUUID().toString(),
@@ -52,32 +52,62 @@ data class SupplierRegistrationDTO (
     val updated: LocalDateTime = LocalDateTime.now(),
     val createdBy: String = REGISTER,
     val updatedBy: String = REGISTER,
-    override val updatedByUser: String="system",
-    val createdByUser: String="system",
-): EventPayload {
-    override fun toRapidDTO(): RapidDTO = SupplierDTO (
-        id = id, status = status, name=name, info = supplierData.toInfo() , identifier = identifier, created = created, updated = updated,
-        createdBy = createdBy, updatedBy = updatedBy)
+    override val updatedByUser: String = "system",
+    val createdByUser: String = "system",
+) : EventPayload {
+    override fun toRapidDTO(): RapidDTO =
+        SupplierDTO(
+            id = id,
+            status = status,
+            name = name,
+            info = supplierData.toInfo(),
+            identifier = identifier,
+            created = created,
+            updated = updated,
+            createdBy = createdBy,
+            updatedBy = updatedBy,
+        )
 
-
-    private fun SupplierData.toInfo(): SupplierInfo = SupplierInfo (
-        address = address, postNr = postNr, postLocation = postLocation, countryCode = countryCode, email = email,
-        phone = phone, homepage = homepage
-    )
-
+    private fun SupplierData.toInfo(): SupplierInfo =
+        SupplierInfo(
+            address = address,
+            postNr = postNr,
+            postLocation = postLocation,
+            countryCode = countryCode,
+            email = email,
+            phone = phone,
+            homepage = homepage,
+        )
 }
 
-fun SupplierRegistration.toDTO(): SupplierRegistrationDTO = SupplierRegistrationDTO (
-    id = id, status = status, draftStatus = draftStatus, name = name, supplierData = supplierData,
-    identifier = identifier, created = created, updated = updated, createdBy = createdBy, updatedBy = updatedBy,
-    updatedByUser = updatedByUser, createdByUser = createdByUser
-)
+fun SupplierRegistration.toDTO(): SupplierRegistrationDTO =
+    SupplierRegistrationDTO(
+        id = id,
+        status = status,
+        draftStatus = draftStatus,
+        name = name,
+        supplierData = supplierData,
+        identifier = identifier,
+        created = created,
+        updated = updated,
+        createdBy = createdBy,
+        updatedBy = updatedBy,
+        updatedByUser = updatedByUser,
+        createdByUser = createdByUser,
+    )
 
-
-
-fun SupplierRegistrationDTO.toEntity(): SupplierRegistration = SupplierRegistration(
-    id = id, status = status, name = name, supplierData = supplierData, identifier = identifier, created = created,
-    updated = updated, createdBy = createdBy, updatedBy=updatedBy, createdByUser = createdByUser,
-    draftStatus = draftStatus, updatedByUser = updatedByUser
-)
-
+fun SupplierRegistrationDTO.toEntity(): SupplierRegistration =
+    SupplierRegistration(
+        id = id,
+        status = status,
+        name = name,
+        supplierData = supplierData,
+        identifier = identifier,
+        created = created,
+        updated = updated,
+        createdBy = createdBy,
+        updatedBy = updatedBy,
+        createdByUser = createdByUser,
+        draftStatus = draftStatus,
+        updatedByUser = updatedByUser,
+    )
