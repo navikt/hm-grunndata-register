55ece805d4487fc06022717e6540ad7792a2e860
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-04-19 13:10:01 +0200

    Update only rows with status INACTIVE

--- src/main/resources/db/V1_34__Patch_series_with_active_prods.sql
+++ src/main/resources/db/V1_34__Patch_series_with_active_prods.sql
@@ -1,6 +1,10 @@
 UPDATE series_reg_v1 s
 SET status = 'ACTIVE'
-WHERE EXISTS(SELECT 1
+WHERE status = 'INACTIVE'
+AND EXISTS(SELECT 1
              FROM product_reg_v1 p
              WHERE p.series_uuid = s.id
                AND (p.registration_status = 'ACTIVE'));
+
+
+SELECT * FROM flyway_schema_history;
