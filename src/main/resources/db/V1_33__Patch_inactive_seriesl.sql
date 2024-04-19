41f52716058246bf116af2538cdbe19042b4c962
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-04-19 11:38:11 +0200

    Remove patch

--- src/main/resources/db/V1_33__Patch_inactive_seriesl.sql
+++
@@ -1,7 +0,0 @@
-UPDATE series_reg_v1 s
-SET status = 'INACTIVE'
-WHERE status != 'INACTIVE'
-  AND NOT EXISTS(SELECT 1
-                 FROM product_reg_v1 p
-                 WHERE p.series_uuid = s.id
-                   AND (p.registration_status = 'ACTIVE' OR p.registration_status = 'DELETED'));
