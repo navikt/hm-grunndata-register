0ab5d3252708c96fe84e50c955f7fde8ec535155
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-05-10 11:28:05 +0200

    Mapping correct adminStatus from HMDB

--- src/main/kotlin/no/nav/hm/grunndata/register/importapi/SeriesImportSyncRiver.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/importapi/SeriesImportSyncRiver.kt
@@ -8,8 +8,12 @@ import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.KafkaRapid
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.River
+import no.nav.hm.grunndata.rapid.dto.AdminStatus
 import no.nav.hm.grunndata.rapid.dto.DraftStatus
+import no.nav.hm.grunndata.rapid.dto.ProductStatus
+import no.nav.hm.grunndata.rapid.dto.RegistrationStatus
 import no.nav.hm.grunndata.rapid.dto.SeriesImportRapidDTO
+import no.nav.hm.grunndata.rapid.dto.SeriesStatus
 import no.nav.hm.grunndata.rapid.dto.rapidDTOVersion
 import no.nav.hm.grunndata.rapid.event.EventName
 import no.nav.hm.grunndata.rapid.event.RapidApp
@@ -17,21 +21,22 @@ import no.nav.hm.grunndata.register.series.SeriesDataDTO
 import no.nav.hm.grunndata.register.series.SeriesRegistrationDTO
 import no.nav.hm.grunndata.register.series.SeriesRegistrationEventHandler
 import no.nav.hm.grunndata.register.series.SeriesRegistrationService
-
 import no.nav.hm.rapids_rivers.micronaut.RiverHead
 import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
 
 @Context
 @Requires(bean = KafkaRapid::class)
-class SeriesImportSyncRiver(river: RiverHead,
-                            private val objectMapper: ObjectMapper,
-                            private val seriesRegistrationService: SeriesRegistrationService,
-                            private val seriesRegistrationEventHandler: SeriesRegistrationEventHandler
-): River.PacketListener {
+class SeriesImportSyncRiver(
+    river: RiverHead,
+    private val objectMapper: ObjectMapper,
+    private val seriesRegistrationService: SeriesRegistrationService,
+    private val seriesRegistrationEventHandler: SeriesRegistrationEventHandler,
+) : River.PacketListener {
     companion object {
         private val LOG = LoggerFactory.getLogger(SeriesImportSyncRiver::class.java)
     }
+
     init {
         river
             .validate { it.demandValue("createdBy", RapidApp.grunndata_import) }
@@ -43,42 +48,60 @@ class SeriesImportSyncRiver(river: RiverHead,
             .register(this)
     }
 
-    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+    override fun onPacket(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
         val eventId = packet["eventId"].asText()
         val dtoVersion = packet["dtoVersion"].asLong()
         if (dtoVersion > rapidDTOVersion) LOG.warn("dto version $dtoVersion is newer than $rapidDTOVersion")
         val dto = objectMapper.treeToValue(packet["payload"], SeriesImportRapidDTO::class.java)
         runBlocking {
-            val series = seriesRegistrationService.findById(dto.id)?.let { inDb ->
-                seriesRegistrationService.update(
-                    inDb.copy(
-                        identifier = dto.id.toString(), title = dto.title, status = dto.status,
-                        updatedBy = "IMPORT", updatedByUser = "IMPORT", seriesData = inDb.seriesData,
-                        updated = LocalDateTime.now(), expired = dto.expired
+            val series =
+                seriesRegistrationService.findById(dto.id)?.let { inDb ->
+                    seriesRegistrationService.update(
+                        inDb.copy(
+                            identifier = dto.id.toString(),
+                            title = dto.title,
+                            status = dto.status,
+                            updatedBy = "IMPORT",
+                            updatedByUser = "IMPORT",
+                            seriesData = inDb.seriesData,
+                            updated = LocalDateTime.now(),
+                            expired = dto.expired,
+                        ),
                     )
+                } ?: seriesRegistrationService.save(
+                    SeriesRegistrationDTO(
+                        id = dto.id,
+                        supplierId = dto.supplierId,
+                        identifier = dto.id.toString(),
+                        title = dto.title,
+                        text = dto.text,
+                        isoCategory = dto.isoCategory,
+                        draftStatus = DraftStatus.DONE,
+                        status = dto.status,
+                        adminStatus = mapAdminStatus(dto.status),
+                        createdBy = "IMPORT",
+                        updatedBy = "IMPORT",
+                        createdByUser = "IMPORT",
+                        updatedByUser = "IMPORT",
+                        seriesData = SeriesDataDTO(media = emptySet()), // import does support media yet.
+                        createdByAdmin = false,
+                    ),
                 )
-            } ?: seriesRegistrationService.save(
-                SeriesRegistrationDTO(
-                    id = dto.id,
-                    supplierId = dto.supplierId,
-                    identifier = dto.id.toString(),
-                    title = dto.title,
-                    text = dto.text,
-                    isoCategory = dto.isoCategory,
-                    draftStatus = DraftStatus.DONE,
-                    status = dto.status,
-                    createdBy = "IMPORT",
-                    updatedBy = "IMPORT",
-                    createdByUser = "IMPORT",
-                    updatedByUser = "IMPORT",
-                    seriesData = SeriesDataDTO(media = emptySet()), //import does support media yet.
-                    createdByAdmin = false
-                )
-            )
             val extraImportKeyValues =
                 mapOf("transferId" to dto.transferId, "version" to dto.version)
-            seriesRegistrationEventHandler.queueDTORapidEvent(series, eventName = EventName.registeredSeriesV1, extraKeyValues = extraImportKeyValues)
+            seriesRegistrationEventHandler.queueDTORapidEvent(
+                series,
+                eventName = EventName.registeredSeriesV1,
+                extraKeyValues = extraImportKeyValues,
+            )
             LOG.info("series import: ${dto.id} transferId: ${dto.transferId} version: ${dto.version} eventId: $eventId synced")
         }
     }
+
+    private fun mapAdminStatus(status: SeriesStatus): AdminStatus =
+        if (status == SeriesStatus.ACTIVE) AdminStatus.APPROVED else AdminStatus.PENDING
+
 }
