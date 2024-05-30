24a3ef818918c0b780e7e77d1574125895ff6504
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-05-30 08:19:42 +0200

    series import river

---
+++ src/main/kotlin/no/nav/hm/grunndata/register/series/SeriesImportRiver.kt
@@ -0,0 +1,92 @@
+package no.nav.hm.grunndata.register.series
+
+import no.nav.hm.grunndata.register.IMPORT
+import no.nav.hm.grunndata.register.product.toMediaInfo
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.micronaut.context.annotation.Context
+import io.micronaut.context.annotation.Requires
+import kotlinx.coroutines.runBlocking
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.KafkaRapid
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.River
+import no.nav.hm.grunndata.rapid.dto.*
+import no.nav.hm.grunndata.rapid.event.EventName
+import no.nav.hm.grunndata.rapid.event.RapidApp
+
+import no.nav.hm.rapids_rivers.micronaut.RiverHead
+import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
+
+@Context
+@Requires(bean = KafkaRapid::class)
+class SeriesImportRiver(
+    river: RiverHead,
+    private val objectMapper: ObjectMapper,
+    private val seriesRegistrationService: SeriesRegistrationService
+) : River.PacketListener {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(SeriesImportRiver::class.java)
+    }
+
+    init {
+        river
+            .validate { it.demandValue("createdBy", RapidApp.grunndata_import) }
+            .validate { it.demandAny("eventName", listOf(EventName.importedSeriesV1)) }
+            .validate { it.demandKey("payload") }
+            .validate { it.demandKey("eventId") }
+            .validate { it.demandKey("dtoVersion") }
+            .validate { it.demandKey("createdTime") }
+            .register(this)
+    }
+
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        val eventId = packet["eventId"].asText()
+        val dtoVersion = packet["dtoVersion"].asLong()
+        if (dtoVersion > rapidDTOVersion) LOG.warn("dto version $dtoVersion is newer than $rapidDTOVersion")
+        val dto = objectMapper.treeToValue(packet["payload"], SeriesImportRapidDTO::class.java)
+        runBlocking {
+            val series = seriesRegistrationService.findById(dto.id)?.let { inDb ->
+                seriesRegistrationService.update(
+                    inDb.copy(
+                        title = dto.title, text= dto.text, isoCategory = dto.isoCategory,
+                        seriesData = SeriesDataDTO(media = dto.seriesData.media.map { it.toMediaInfo() }.toSet(),
+                            attributes = SeriesAttributesDTO(
+                            keywords = dto.seriesData.attributes.keywords)
+                        ),
+                        status = dto.status,
+                        updatedBy = IMPORT,
+                        updatedByUser = IMPORT,
+                        updated = LocalDateTime.now()
+                    )
+                )
+            } ?: seriesRegistrationService.save(
+                SeriesRegistrationDTO(
+                    id = dto.id,
+                    supplierId = dto.supplierId,
+                    identifier = dto.id.toString(),
+                    title = dto.title,
+                    text = dto.text,
+                    isoCategory = dto.isoCategory,
+                    draftStatus = DraftStatus.DONE,
+                    status = dto.status,
+                    seriesData = SeriesDataDTO(
+                        media = dto.seriesData.media.map { it.toMediaInfo() }.toSet(),
+                        attributes = SeriesAttributesDTO(
+                            keywords = dto.seriesData.attributes.keywords
+                        )
+                    ),
+                    createdBy = IMPORT,
+                    updatedBy = IMPORT,
+                    createdByUser = IMPORT,
+                    updatedByUser = IMPORT,
+                    createdByAdmin = false
+                )
+            )
+            LOG.info("series import id: ${series.id} with eventId $eventId synced")
+        }
+    }
+}
+
