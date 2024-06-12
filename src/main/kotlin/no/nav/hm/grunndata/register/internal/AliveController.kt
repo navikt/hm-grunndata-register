e3c94707160ce9b5e9c0c770314a61d621ad0889
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-06-12 14:13:22 +0200

    remove bq

--- src/main/kotlin/no/nav/hm/grunndata/register/internal/AliveController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/internal/AliveController.kt
@@ -5,20 +5,16 @@ import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 import io.swagger.v3.oas.annotations.Hidden
-import no.nav.hm.grunndata.register.eventstore.BigQueryService
 
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/internal")
 @Hidden
-class AliveController(private val bigQueryService: BigQueryService) {
+class AliveController() {
 
     @Get("/isAlive")
     fun alive() = "ALIVE"
 
     @Get("/isReady")
     fun ready() = "OK"
-
-    @Get("/bq/check") // For testing purposes
-    fun bigQuery() = bigQueryService.checkDataSet()
 
 }
