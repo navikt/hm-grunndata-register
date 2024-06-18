7b818b00973d4193bd610c5232004ae01311e8ad
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-06-18 10:39:13 +0200

    added a scheduler to delete old product versions

---
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/ProductRegistrationVersionScheduler.kt
@@ -0,0 +1,20 @@
+package no.nav.hm.grunndata.register.product
+
+import io.micronaut.context.annotation.Requires
+import io.micronaut.scheduling.annotation.Scheduled
+import jakarta.inject.Singleton
+import kotlinx.coroutines.runBlocking
+import no.nav.hm.grunndata.register.leaderelection.LeaderOnly
+
+@Singleton
+@Requires(property = "schedulers.enabled", value = "true")
+open class ProductRegistrationVersionScheduler(
+    private val productRegistrationVersionService: ProductRegistrationVersionService) {
+
+    @LeaderOnly
+    @Scheduled(cron = "0 0 1 * * *")
+    open fun deleteOldSeriesRegistrationVersions() = runBlocking {
+        productRegistrationVersionService.deleteOldVersions()
+    }
+
+}
