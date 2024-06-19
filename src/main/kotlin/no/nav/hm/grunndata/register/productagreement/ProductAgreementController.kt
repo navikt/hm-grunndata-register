5a267b85909f1fdfc88b64ad4164fbd764f9d4a9
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-06-19 12:54:27 +0200

    Endpoint for checking if series is part of an agreement

---
+++ src/main/kotlin/no/nav/hm/grunndata/register/productagreement/ProductAgreementController.kt
@@ -0,0 +1,46 @@
+package no.nav.hm.grunndata.register.productagreement
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.authentication.Authentication
+import io.swagger.v3.oas.annotations.tags.Tag
+import no.nav.hm.grunndata.rapid.dto.RegistrationStatus
+import no.nav.hm.grunndata.register.product.ProductRegistrationService
+import no.nav.hm.grunndata.register.security.Roles
+import org.slf4j.LoggerFactory
+import java.util.UUID
+
+@Secured(Roles.ROLE_SUPPLIER)
+@Controller(ProductAgreementController.VENDOR_API_V1_PRODUCT_AGREEMENT)
+@Tag(name = "Vendor Product Agreement")
+class ProductAgreementController(
+    private val productAgreementRegistrationService: ProductAgreementRegistrationService,
+    private val productRegistrationService: ProductRegistrationService,
+) {
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ProductAgreementAdminController::class.java)
+        const val VENDOR_API_V1_PRODUCT_AGREEMENT = "/vendor/api/v1/product-agreement"
+    }
+
+    @Get(
+        value = "/in-agreement/{seriesId}",
+        consumes = [io.micronaut.http.MediaType.MULTIPART_FORM_DATA],
+        produces = [io.micronaut.http.MediaType.APPLICATION_JSON],
+    )
+    suspend fun isSeriesInAgreement(
+        seriesId: UUID,
+        authentication: Authentication,
+    ): Boolean {
+        LOG.info("Getting variants for series {$seriesId}")
+        val ids: List<UUID> =
+            productRegistrationService.findAllBySeriesUuid(seriesId)
+                .filter {
+                    it.registrationStatus == RegistrationStatus.ACTIVE
+                }.map { it.id }
+
+        val productAgreements = productAgreementRegistrationService.findAllByIds(ids)
+
+        return productAgreements.isNotEmpty()
+    }
+}
