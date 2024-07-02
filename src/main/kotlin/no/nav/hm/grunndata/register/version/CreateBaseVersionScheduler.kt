a235243b7b99c5773cc182a893b9b54ac7e574ef
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-07-02 09:47:59 +0200

    Scheduler that creates base versions for migrated products for a list of migrated suppliers.

--- src/main/kotlin/no/nav/hm/grunndata/register/version/CreateBaseVersionScheduler.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/version/CreateBaseVersionScheduler.kt
@@ -1,8 +1,7 @@
 package no.nav.hm.grunndata.register.version
 
 import io.micronaut.context.annotation.Requires
-import io.micronaut.context.event.StartupEvent
-import io.micronaut.runtime.event.annotation.EventListener
+import io.micronaut.scheduling.annotation.Scheduled
 import jakarta.inject.Singleton
 import kotlinx.coroutines.runBlocking
 import no.nav.hm.grunndata.register.leaderelection.LeaderOnly
@@ -15,12 +14,11 @@ open class CreateBaseVersionScheduler(private val createBaseVersionHandler: Crea
         private val LOG = LoggerFactory.getLogger(CreateBaseVersionScheduler::class.java)
     }
 
-    @EventListener
     @LeaderOnly
-    open fun init(event: StartupEvent?) {
+    @Scheduled(fixedDelay = "4m")
+    open fun scheduleProductExpirePublish() =
         runBlocking {
             LOG.info("Running ProductExpirePublishScheduler")
             createBaseVersionHandler.createVersionsWhereMissingForMigratedSuppliers()
         }
-    }
 }
