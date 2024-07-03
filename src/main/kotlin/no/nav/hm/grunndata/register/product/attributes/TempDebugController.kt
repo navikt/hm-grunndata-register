c66bd5ce237b5bc57efad52eb867ccd41935de5e
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-07-03 13:40:06 +0200

    Added temp debug controller to trigger scheduled jobs on-demand

---
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/attributes/TempDebugController.kt
@@ -0,0 +1,52 @@
+package no.nav.hm.grunndata.register.product.attributes
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import io.micronaut.security.annotation.Secured
+import io.swagger.v3.oas.annotations.tags.Tag
+import no.nav.hm.grunndata.register.product.attributes.bestillingsordning.BestillingsordningService
+import no.nav.hm.grunndata.register.product.attributes.digitalsoknadsortiment.DigitalSoknadSortimentService
+import no.nav.hm.grunndata.register.product.attributes.paakrevdgodkjenningskurs.PaakrevdGodkjenningskursService
+import no.nav.hm.grunndata.register.product.attributes.produkttype.ProdukttypeService
+import no.nav.hm.grunndata.register.security.Roles
+import org.slf4j.LoggerFactory
+
+@Controller(TempDebugController.API_V1_TEMP_TEST_BASEURL)
+@Secured(Roles.ROLE_ADMIN)
+@Tag(name="temp test")
+class TempDebugController(
+    private val bestillingsordningService: BestillingsordningService,
+    private val digitalSoknadSortimentService: DigitalSoknadSortimentService,
+    private val paakrevdGodkjenningskursService: PaakrevdGodkjenningskursService,
+    private val produkttypeService: ProdukttypeService,
+) {
+    companion object {
+        private val LOG = LoggerFactory.getLogger(TempDebugController::class.java)
+        const val API_V1_TEMP_TEST_BASEURL = "/temp-test"
+    }
+
+    @Post("/bestillingsordning")
+    suspend fun bestillingsordning(): HttpResponse<String> {
+        bestillingsordningService.importAndUpdateDb()
+        return HttpResponse.ok("done.")
+    }
+
+    @Post("/digitalSoknadSortiment")
+    suspend fun digitalSoknadSortiment(): HttpResponse<String> {
+        digitalSoknadSortimentService.importAndUpdateDb()
+        return HttpResponse.ok("done.")
+    }
+
+    @Post("/paakrevdGodkjenningskurs")
+    suspend fun paakrevdGodkjenningskurs(): HttpResponse<String> {
+        paakrevdGodkjenningskursService.importAndUpdateDb()
+        return HttpResponse.ok("done.")
+    }
+
+    @Post("/produkttype")
+    suspend fun produkttype(): HttpResponse<String> {
+        produkttypeService.importAndUpdateDb()
+        return HttpResponse.ok("done.")
+    }
+}
