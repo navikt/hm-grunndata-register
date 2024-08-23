52fc8c63fd7a380c4d8654276f92d2b184c3e801
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-08-23 13:48:27 +0200

    more logs for debug

--- src/main/kotlin/no/nav/hm/grunndata/register/event/EventItemScheduler.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/event/EventItemScheduler.kt
@@ -25,7 +25,7 @@ open class EventItemScheduler(
             val items = eventItemService.getAllPendingStatus().sortedBy { it.updated }
             LOG.info("Running sendEventItemScheduler with ${items.size} items")
             items.forEach {
-                LOG.info("sending event ${it.oid} with type ${it.type}")
+                LOG.info("sending event ${it.oid} with type ${it.type} and ${it.updated}")
                 eventItemService.sendRapidEvent(it)
             }
         }
