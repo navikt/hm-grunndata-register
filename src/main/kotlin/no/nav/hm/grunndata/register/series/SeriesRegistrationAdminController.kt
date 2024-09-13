e14cb5779fb7fe277cbcae1391232431829ccaa9
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2024-09-13 09:58:29 +0200

    Add created by user filter

--- src/main/kotlin/no/nav/hm/grunndata/register/series/SeriesRegistrationAdminController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/series/SeriesRegistrationAdminController.kt
@@ -132,6 +132,15 @@ class SeriesRegistrationAdminController(
                     )
                 }
 
+                if (it.containsKey("createdByAdmin")) {
+                    predicates.add(
+                        criteriaBuilder.equal(
+                            root[SeriesRegistration::createdByAdmin],
+                            it["createdByAdmin"].toBoolean()
+                        )
+                    )
+                }
+
                 if (inputparams.contains("supplierFilter")) {
                     val supplierList: List<UUID> =
                         inputparams["supplierFilter"]!!.split(",").map { UUID.fromString(it) }
