279d7413a2b166057e3670a75cd509e579eabcac
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-09-20 09:39:56 +0200

    upgrade dto lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val poiVersion = "5.2.5"
 val rapidsRiversVersion = "202408280906"
-val grunndataDtoVersion = "202409131454"
+val grunndataDtoVersion = "202409181446"
 val microsoftGrapVersion = "5.77.0"
 
 group = "no.nav.hm"
