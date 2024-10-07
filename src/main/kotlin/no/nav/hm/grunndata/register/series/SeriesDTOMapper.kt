a0b2d3f4ea2f8e2ac7068d0a62044eaf95ba224f
Marte Pedersen <Marte.Pedersen@nav.no>
2024-10-07 15:54:34 +0200

    Nytt endepunkt for oppdatering av serie (#50)
    * start på å lage nytt endepunkt for oppdatering av serie-informasjon

---
+++ src/main/kotlin/no/nav/hm/grunndata/register/series/SeriesDTOMapper.kt
@@ -0,0 +1,105 @@
+package no.nav.hm.grunndata.register.series
+
+import jakarta.inject.Singleton
+import java.time.LocalDateTime
+import no.nav.hm.grunndata.rapid.dto.RegistrationStatus
+import no.nav.hm.grunndata.register.HMDB
+import no.nav.hm.grunndata.register.iso.IsoCategoryService
+import no.nav.hm.grunndata.register.product.ProductDTOMapper
+import no.nav.hm.grunndata.register.product.ProductRegistrationService
+import no.nav.hm.grunndata.register.productagreement.ProductAgreementRegistrationService
+import no.nav.hm.grunndata.register.supplier.SupplierRegistrationService
+
+@Singleton
+class SeriesDTOMapper(
+    private val productAgreementRegistrationService: ProductAgreementRegistrationService,
+    private val productRegistrationService: ProductRegistrationService,
+    private val isoCategoryService: IsoCategoryService,
+    private val supplierRegistrationService: SupplierRegistrationService,
+    private val productDTOMapper: ProductDTOMapper
+) {
+
+    suspend fun toDTOV2(seriesRegistration: SeriesRegistration): SeriesRegistrationDTOV2 {
+        val supplierName = supplierRegistrationService.findById(seriesRegistration.supplierId)?.name ?: ""
+        val isoCategoryDTO = isoCategoryService.lookUpCode(seriesRegistration.isoCategory)
+        val productRegistrationDTOs = productRegistrationService.findAllBySeriesUuid(seriesRegistration.id)
+            .map { product -> productDTOMapper.toDTOV2(product) }
+        val inAgreement = productAgreementRegistrationService.findAllByProductIds(
+            productRegistrationService.findAllBySeriesUuid(seriesRegistration.id)
+                .filter { it.registrationStatus == RegistrationStatus.ACTIVE }
+                .map { it.id },
+        ).isNotEmpty()
+
+
+        return SeriesRegistrationDTOV2(
+            id = seriesRegistration.id,
+            supplierName = supplierName,
+            title = seriesRegistration.title,
+            text = seriesRegistration.text,
+            isoCategory = isoCategoryDTO,
+            message = seriesRegistration.message,
+            status = EditStatus.from(seriesRegistration),
+            seriesData = seriesRegistration.seriesData,
+            created = seriesRegistration.created,
+            updated = seriesRegistration.updated,
+            published = seriesRegistration.published,
+            expired = seriesRegistration.expired,
+            updatedByUser = seriesRegistration.updatedByUser,
+            createdByUser = seriesRegistration.createdByUser,
+            variants = productRegistrationDTOs,
+            version = seriesRegistration.version,
+            isExpired = seriesRegistration.expired < LocalDateTime.now(),
+            isPublished = seriesRegistration.published?.let { it < LocalDateTime.now() } ?: false,
+            inAgreement = inAgreement,
+            hmdbId = if (seriesRegistration.identifier != seriesRegistration.id.toString() &&
+                seriesRegistration.updatedBy == HMDB
+            ) {
+                seriesRegistration.identifier
+            } else {
+                null
+            },
+        )
+    }
+
+    suspend fun toDTOV2(seriesRegistrationDTO: SeriesRegistrationDTO): SeriesRegistrationDTOV2 {
+        val supplierName = supplierRegistrationService.findById(seriesRegistrationDTO.supplierId)?.name ?: ""
+        val isoCategoryDTO = isoCategoryService.lookUpCode(seriesRegistrationDTO.isoCategory)
+        val productRegistrationDTOs = productRegistrationService.findAllBySeriesUuid(seriesRegistrationDTO.id)
+            .map { product -> productDTOMapper.toDTOV2(product) }
+        val inAgreement = productAgreementRegistrationService.findAllByProductIds(
+            productRegistrationService.findAllBySeriesUuid(seriesRegistrationDTO.id)
+                .filter { it.registrationStatus == RegistrationStatus.ACTIVE }
+                .map { it.id },
+        ).isNotEmpty()
+
+
+        return SeriesRegistrationDTOV2(
+            id = seriesRegistrationDTO.id,
+            supplierName = supplierName,
+            title = seriesRegistrationDTO.title,
+            text = seriesRegistrationDTO.text,
+            isoCategory = isoCategoryDTO,
+            message = seriesRegistrationDTO.message,
+            status = EditStatus.from(seriesRegistrationDTO),
+            seriesData = seriesRegistrationDTO.seriesData,
+            created = seriesRegistrationDTO.created,
+            updated = seriesRegistrationDTO.updated,
+            published = seriesRegistrationDTO.published,
+            expired = seriesRegistrationDTO.expired,
+            updatedByUser = seriesRegistrationDTO.updatedByUser,
+            createdByUser = seriesRegistrationDTO.createdByUser,
+            variants = productRegistrationDTOs,
+            version = seriesRegistrationDTO.version,
+            isExpired = seriesRegistrationDTO.expired < LocalDateTime.now(),
+            isPublished = seriesRegistrationDTO.published?.let { it < LocalDateTime.now() } ?: false,
+            inAgreement = inAgreement,
+            hmdbId = if (seriesRegistrationDTO.identifier != seriesRegistrationDTO.id.toString() &&
+                seriesRegistrationDTO.updatedBy == HMDB
+            ) {
+                seriesRegistrationDTO.identifier
+            } else {
+                null
+            },
+        )
+    }
+}
--- src/main/kotlin/no/nav/hm/grunndata/register/series/SeriesRegistrationController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/series/SeriesRegistrationController.kt
@@ -11,6 +11,7 @@ import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Delete
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Patch
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Put
@@ -37,7 +38,10 @@ import org.reactivestreams.Publisher
 @Secured(Roles.ROLE_SUPPLIER)
 @Controller(SeriesRegistrationController.API_V1_SERIES)
 @Tag(name = "Vendor Series")
-class SeriesRegistrationController(private val seriesRegistrationService: SeriesRegistrationService) {
+class SeriesRegistrationController(
+    private val seriesRegistrationService: SeriesRegistrationService,
+    private val seriesDTOMapper: SeriesDTOMapper
+) {
     companion object {
         private val LOG = LoggerFactory.getLogger(SeriesRegistrationController::class.java)
         const val API_V1_SERIES = "/vendor/api/v1/series"
@@ -179,6 +183,22 @@ class SeriesRegistrationController(private val seriesRegistrationService: Series
             HttpResponse.notFound()
         }
 
+    @Patch("/v2/{id}")
+    suspend fun patchSeriesV2(
+        @PathVariable id: UUID,
+        @Body updateSeriesRegistrationDTO: UpdateSeriesRegistrationDTO,
+        authentication: Authentication,
+    ): HttpResponse<SeriesRegistrationDTOV2> =
+        HttpResponse.ok(
+            seriesDTOMapper.toDTOV2(
+                seriesRegistrationService.patchSeries(
+                    id,
+                    updateSeriesRegistrationDTO,
+                    authentication
+                )
+            )
+        )
+
     @Post(
         value = "/uploadMedia/{seriesUUID}",
         consumes = [MediaType.MULTIPART_FORM_DATA],
@@ -198,7 +218,7 @@ class SeriesRegistrationController(private val seriesRegistrationService: Series
 
         LOG.info("supplier: ${authentication.supplierId()} uploading files for series $seriesUUID")
         seriesRegistrationService.uploadMediaAndUpdateSeries(seriesToUpdate, files)
-        
+
         return HttpResponse.ok()
     }
 
