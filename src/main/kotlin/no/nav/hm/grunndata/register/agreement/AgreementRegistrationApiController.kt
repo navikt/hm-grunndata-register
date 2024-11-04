7c1bd6788525ffcade37918ed737e9497be225bd
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-11-04 08:34:28 +0100

    fix agreementcontroller for leverandor

--- src/main/kotlin/no/nav/hm/grunndata/register/agreement/AgreementRegistrationApiController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/agreement/AgreementRegistrationApiController.kt
@@ -3,14 +3,15 @@ package no.nav.hm.grunndata.register.agreement
 import io.micronaut.core.annotation.Introspected
 import io.micronaut.data.model.Page
 import io.micronaut.data.model.Pageable
+import io.micronaut.data.model.jpa.criteria.impl.expression.LiteralExpression
 import io.micronaut.data.repository.jpa.criteria.PredicateSpecification
 import io.micronaut.data.runtime.criteria.get
-import io.micronaut.data.runtime.criteria.where
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.RequestBean
 import io.micronaut.security.annotation.Secured
 import io.swagger.v3.oas.annotations.tags.Tag
+import no.nav.hm.grunndata.register.runtime.where
 import no.nav.hm.grunndata.register.security.Roles
 import org.slf4j.LoggerFactory
 
@@ -33,10 +34,9 @@ class AgreementRegistrationApiController(private val agreementRegistrationServic
         if (criteria.isNotEmpty()) {
             where {
                 criteria.reference?.let { root[AgreementRegistration::reference] eq it }
-            }.and { root, criteriaBuilder ->
-                if (criteria.title != null) {
-                    criteriaBuilder.like(root[AgreementRegistration::title], criteriaBuilder.literal("%${criteria.title}%"))
-                } else null
+                criteria.title?.let {
+                    root[AgreementRegistration::title] like LiteralExpression("%${criteria.title}%")
+                }
             }
         } else null
 
