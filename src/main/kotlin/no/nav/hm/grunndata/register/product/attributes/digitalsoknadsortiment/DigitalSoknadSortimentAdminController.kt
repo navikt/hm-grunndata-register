ac62ec12853b2782fac3f30344b78916691af2c4
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-11-26 10:13:03 +0100

    Remove remaining authentication param

--- src/main/kotlin/no/nav/hm/grunndata/register/product/attributes/digitalsoknadsortiment/DigitalSoknadSortimentAdminController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/attributes/digitalsoknadsortiment/DigitalSoknadSortimentAdminController.kt
@@ -9,19 +9,17 @@ import io.micronaut.security.rules.SecurityRule
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.LoggerFactory
 
-
 @Controller(DigitalSoknadSortimentAdminController.API_V1_INTERNAL_DIGITALSOKNADSORTIMENT_REGISTRATIONS)
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Tag(name="Admin Digital Soknad")
 class DigitalSoknadSortimentAdminController(private val digitalSoknadSortimentService: DigitalSoknadSortimentService) {
-
     companion object {
         private val LOG = LoggerFactory.getLogger(DigitalSoknadSortimentAdminController::class.java)
         const val API_V1_INTERNAL_DIGITALSOKNADSORTIMENT_REGISTRATIONS = "/internal/api/v1/digitalsoknadsortiment/registrations"
     }
 
     @Post("/manual-sync")
-    suspend fun manualSync(authentication: Authentication): HttpResponse<Unit> =
+    suspend fun manualSync(): HttpResponse<Unit> =
         digitalSoknadSortimentService.importAndUpdateDb().run {
             HttpResponse.ok()
         }
