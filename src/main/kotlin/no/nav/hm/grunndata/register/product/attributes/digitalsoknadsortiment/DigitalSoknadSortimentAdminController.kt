19fb3d1049a7f5a399dc16ad218009bbc492bd3e
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-11-26 10:13:36 +0100

    Optimize imports

--- src/main/kotlin/no/nav/hm/grunndata/register/product/attributes/digitalsoknadsortiment/DigitalSoknadSortimentAdminController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/attributes/digitalsoknadsortiment/DigitalSoknadSortimentAdminController.kt
@@ -4,7 +4,6 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
 import io.micronaut.security.annotation.Secured
-import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.LoggerFactory
