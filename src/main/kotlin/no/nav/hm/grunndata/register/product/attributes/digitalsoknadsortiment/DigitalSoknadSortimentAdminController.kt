4b1ed358bb38ee4194c82c1f2b7af23ba1fa5c15
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-11-26 10:03:04 +0100

    Move new api to internal and anonymous security

--- src/main/kotlin/no/nav/hm/grunndata/register/product/attributes/digitalsoknadsortiment/DigitalSoknadSortimentAdminController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/product/attributes/digitalsoknadsortiment/DigitalSoknadSortimentAdminController.kt
@@ -5,19 +5,20 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
+import io.micronaut.security.rules.SecurityRule
 import io.swagger.v3.oas.annotations.tags.Tag
 import no.nav.hm.grunndata.register.security.Roles
 import org.slf4j.LoggerFactory
 
 
-@Controller(DigitalSoknadSortimentAdminController.API_V1_ADMIN_DIGITALSOKNADSORTIMENT_REGISTRATIONS)
-@Secured(Roles.ROLE_ADMIN)
+@Controller(DigitalSoknadSortimentAdminController.API_V1_INTERNAL_DIGITALSOKNADSORTIMENT_REGISTRATIONS)
+@Secured(SecurityRule.IS_ANONYMOUS)
 @Tag(name="Admin Digital Soknad")
 class DigitalSoknadSortimentAdminController(private val digitalSoknadSortimentService: DigitalSoknadSortimentService) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(DigitalSoknadSortimentAdminController::class.java)
-        const val API_V1_ADMIN_DIGITALSOKNADSORTIMENT_REGISTRATIONS = "/admin/api/v1/digitalsoknadsortiment/registrations"
+        const val API_V1_INTERNAL_DIGITALSOKNADSORTIMENT_REGISTRATIONS = "/internal/api/v1/digitalsoknadsortiment/registrations"
     }
 
     @Post("/manual-sync")
