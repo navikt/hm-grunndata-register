3de3f75794be3909baad83a3c5152ea44b152e3f
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-01-21 13:22:47 +0100

    added function to connect compatible products for the whole catalog

--- src/main/kotlin/no/nav/hm/grunndata/register/internal/maintenance/CompatibleProductController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/internal/maintenance/CompatibleProductController.kt
@@ -7,6 +7,8 @@ import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 import io.swagger.v3.oas.annotations.Hidden
 import no.nav.hm.grunndata.register.accessory.CompatibleWithFinder
+import no.nav.hm.grunndata.register.catalog.CatalogImportRepository
+import no.nav.hm.grunndata.register.catalog.CatalogImportService
 import no.nav.hm.grunndata.register.product.ProductRegistrationService
 import org.slf4j.LoggerFactory
 
@@ -14,10 +16,11 @@ import org.slf4j.LoggerFactory
 @Controller("/internal/compatible/products")
 @Hidden
 class CompatibleProductController(private val compatibleWithFinder: CompatibleWithFinder,
+                                  private val catalogImportRepository: CatalogImportRepository,
                                   private val productRegistrationService: ProductRegistrationService) {
 
-    @Post("/connect")
-    suspend fun connect(@QueryValue hmsNr: String) {
+    @Post("/connect/{hmsNr}")
+    suspend fun connect(hmsNr: String) {
         productRegistrationService.findByHmsArtNr(hmsNr)?.let { product ->
             compatibleWithFinder.addCompatibleWithAttributeLink(product).let { updatedProduct ->
                 productRegistrationService.saveAndCreateEventIfNotDraftAndApproved(updatedProduct, isUpdate = true)
@@ -29,5 +32,14 @@ class CompatibleProductController(private val compatibleWithFinder: CompatibleWi
         private val LOG = LoggerFactory.getLogger(CompatibleProductController::class.java)
     }
 
-
+    @Post("/connect/orderref/{orderRef}")
+    suspend fun connectWithOrderRef(orderRef: String) {
+        catalogImportRepository.findCatalogSeriesInfoByOrderRef(orderRef).filter { !it.mainProduct && it.productId != null }.forEach {
+            productRegistrationService.findById(it.productId!!)?.let { product ->
+                compatibleWithFinder.addCompatibleWithAttributeLink(product).let { updatedProduct ->
+                    productRegistrationService.saveAndCreateEventIfNotDraftAndApproved(updatedProduct, isUpdate = true)
+                }
+            }
+        }
+    }
 }
