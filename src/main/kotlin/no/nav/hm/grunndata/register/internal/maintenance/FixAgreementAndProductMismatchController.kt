4e2bb76c0749fca3c88be924014f16ee8d2ba57a
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-01-29 12:49:47 +0100

    copy the hmsnr from the catalog

--- src/main/kotlin/no/nav/hm/grunndata/register/internal/maintenance/FixAgreementAndProductMismatchController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/internal/maintenance/FixAgreementAndProductMismatchController.kt
@@ -5,6 +5,7 @@ import io.micronaut.http.annotation.Post
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 import io.swagger.v3.oas.annotations.Hidden
+import no.nav.hm.grunndata.rapid.dto.ProductAgreementStatus
 import no.nav.hm.grunndata.register.product.ProductRegistrationRepository
 import no.nav.hm.grunndata.register.product.ProductRegistrationService
 import no.nav.hm.grunndata.register.productagreement.ProductAgreementRegistrationRepository
@@ -21,15 +22,20 @@ class FixAgreementAndProductMismatchController(private val productRegistrationRe
     suspend fun fixAgreementAndProductMisMatchForSparePartAndAccessory() {
         val products =  productRegistrationRepository.findProductThatDoesNotMatchAgreementSparePartAccessory()
         LOG.info("Got ${products.size} products that does not match agreement and spare part/accessory")
-        products.forEach {
-            LOG.info("Fixing product ${it.id} for supplier ${it.supplierId}")
-            val pag = productAgreementRegistrationRepository.findBySupplierIdAndSupplierRef(it.supplierId, it.supplierRef).first()
-            productRegistrationService.saveAndCreateEventIfNotDraftAndApproved(
-                it.copy(
-                    accessory = pag.accessory,
-                    sparePart = pag.sparePart
-                ), isUpdate = true
-            )
+        products.forEach { product ->
+            LOG.info("Fixing product ${product.id} for supplier ${product.supplierId}")
+            val pag =
+                productAgreementRegistrationRepository.findBySupplierIdAndSupplierRef(product.supplierId, product.supplierRef)
+                    .firstOrNull { it.status == ProductAgreementStatus.ACTIVE }
+            pag?.let {
+                productRegistrationService.saveAndCreateEventIfNotDraftAndApproved(
+                    product.copy(
+                        hmsArtNr = pag.hmsArtNr,
+                        accessory = pag.accessory,
+                        sparePart = pag.sparePart
+                    ), isUpdate = true
+                )
+            }
         }
     }
 
