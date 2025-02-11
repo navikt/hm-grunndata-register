a2d9aa3065f618a284a741738285d187be9597c8
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-02-11 10:20:51 +0100

    upgrade micronaut 4.7.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
-val micronautVersion = "4.7.0"
+val micronautVersion = "4.7.5"
 val junitJupiterVersion = "5.9.0"
 val logbackEncoderVersion = "7.3"
 val postgresqlVersion = "42.7.2"
@@ -23,7 +23,7 @@ plugins {
     kotlin("kapt") version "1.9.25"
     id("java")
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("io.micronaut.application") version "4.4.4"
+    id("io.micronaut.application") version "4.4.5"
 }
 
 configurations.all {
