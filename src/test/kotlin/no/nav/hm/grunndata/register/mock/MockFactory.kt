ed95f724a1c6a761132520ecf8b63a210dec0be7
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-02-19 09:05:01 +0100

    removed gdb client, no need

--- src/test/kotlin/no/nav/hm/grunndata/register/mock/MockFactory.kt
+++ src/test/kotlin/no/nav/hm/grunndata/register/mock/MockFactory.kt
@@ -1,18 +1,15 @@
 package no.nav.hm.grunndata.register.mock
 
 import io.micronaut.context.annotation.Factory
-import io.micronaut.test.annotation.MockBean
 import io.mockk.mockk
 import jakarta.inject.Singleton
-import no.nav.hm.grunndata.register.gdb.GdbApiClient
+
 import no.nav.hm.rapids_rivers.micronaut.RapidPushService
 
 
 @Factory
 class MockFactory {
 
-    @Singleton
-    fun mockGdbClient(): GdbApiClient = mockk(relaxed = true)
 
     @Singleton
     fun rapidPushService(): RapidPushService = mockk(relaxed = true)
