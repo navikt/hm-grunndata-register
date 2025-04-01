0f65005c09501ce81ed4bb713f9d5c932335c4e9
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-04-01 09:57:14 +0200

    default to true for mainproduct bool

--- src/main/resources/db/V1_77__Add_mainproduct_to_product.sql
+++ src/main/resources/db/V1_77__Add_mainproduct_to_product.sql
@@ -1,5 +1,5 @@
 ALTER TABLE product_reg_v1
-    ADD COLUMN main_product BOOLEAN;
+    ADD COLUMN main_product BOOLEAN NOT NULL DEFAULT true;
 UPDATE product_reg_v1
 SET main_product = CASE
     WHEN accessory = false AND spare_part = false THEN true
