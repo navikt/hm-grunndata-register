6032f476bba19eef507badb28b794a2a228d3e44
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-10-24 09:46:06 +0200

    improve duplicate hmsnr handling

--- src/main/kotlin/no/nav/hm/grunndata/register/internal/maintenance/DuplicateHmsnrSupplierIdHandlerController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/internal/maintenance/DuplicateHmsnrSupplierIdHandlerController.kt
@@ -17,20 +17,20 @@ import java.time.LocalDateTime
 @Hidden
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/internal/duplicate/hmsnr")
-class DuplicateHmsnrHandlerController(private val productRegistrationRepository: ProductRegistrationRepository,
-                                      private val productAgreementRegistrationRepository: ProductAgreementRegistrationRepository,
-                                      private val productRegistrationService: ProductRegistrationService) {
+class DuplicateHmsnrSupplierIdHandlerController(private val productRegistrationRepository: ProductRegistrationRepository,
+                                                private val productAgreementRegistrationRepository: ProductAgreementRegistrationRepository,
+                                                private val productRegistrationService: ProductRegistrationService) {
 
     @Get("/")
     suspend fun findDuplicateHmsnr(): List<ProductIdHmsArtNr> {
-        val duplicates = productRegistrationRepository.findAllDuplicateHmsArtnr()
+        val duplicates = productRegistrationRepository.findAllDuplicateHmsArtnrSupplierId()
         LOG.info("Found ${duplicates.size} duplicates")
         return duplicates
     }
 
     @Delete("/delete")
     suspend fun findAndDeleteDuplicateHmsnr(): List<ProductIdHmsArtNr> {
-        val duplicates = productRegistrationRepository.findAllDuplicateHmsArtnr()
+        val duplicates = productRegistrationRepository.findAllDuplicateHmsArtnrSupplierId()
         duplicates.forEach { duplicate ->
             if (duplicate.hmsArtNr.isNotEmpty()) {
                 productRegistrationService.findById(duplicate.id)?.let { product ->
@@ -52,6 +52,6 @@ class DuplicateHmsnrHandlerController(private val productRegistrationRepository:
         return duplicates
     }
     companion object {
-        private val LOG = org.slf4j.LoggerFactory.getLogger(DuplicateHmsnrHandlerController::class.java)
+        private val LOG = org.slf4j.LoggerFactory.getLogger(DuplicateHmsnrSupplierIdHandlerController::class.java)
     }
 }
