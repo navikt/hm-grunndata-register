a364e0c4abbca7044c043b117e95c075e60c3c92
Aasmund Nordstoga <anostoga@gmail.com>
2025-12-16 13:50:27 +0100

    Revert "Support d6Br2 style import"
    This reverts commit 266ca765fd757ff1b2e4266df077a667de64c98f.

--- src/main/kotlin/no/nav/hm/grunndata/register/catalog/ProductAgreementImportExcelService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/catalog/ProductAgreementImportExcelService.kt
@@ -280,7 +280,7 @@ fun parsedelkontraktNr(subContractNr: String): List<Pair<String, Int>> {
                 matchResult = matchResult.next()
             }
         } else {
-            throw BadRequestException("Klarte ikke å lese delkontrakt nr. $subContractNr, Clan subcontractnr: $cleanSubContractNr" )
+            throw BadRequestException("Klarte ikke å lese delkontrakt nr. $subContractNr")
         }
         return mutableList
     } catch (e: Exception) {
@@ -314,7 +314,4 @@ data class ProductAgreementMappedResultLists(
     val deactivateList: List<ProductAgreementRegistrationDTO> = emptyList(),
 )
 
-val delKontraktRegex = Regex(
-    pattern = "d(\\d+)([A-Z]*)r?(\\d*)[,]?",
-    options = setOf(RegexOption.IGNORE_CASE)
-)
+val delKontraktRegex = Regex("d(\\d+)([A-Q-STU-Z]*)r*(\\d*),*")
