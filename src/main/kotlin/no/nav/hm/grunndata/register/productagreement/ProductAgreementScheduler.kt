22bd5e7465cc844613aaa6c4961e3fc9d58f3e90
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2026-05-05 08:54:43 +0200

    improve productagreement scheduler

--- src/main/kotlin/no/nav/hm/grunndata/register/productagreement/ProductAgreementScheduler.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/productagreement/ProductAgreementScheduler.kt
@@ -5,14 +5,22 @@ import io.micronaut.scheduling.annotation.Scheduled
 import jakarta.inject.Singleton
 import java.time.LocalDateTime
 import kotlinx.coroutines.runBlocking
+import no.nav.hm.grunndata.rapid.dto.AgreementStatus
+import no.nav.hm.grunndata.rapid.dto.DraftStatus
 import no.nav.hm.grunndata.rapid.dto.ProductAgreementStatus
 import no.nav.hm.grunndata.register.REGISTER
+import no.nav.hm.grunndata.register.agreement.AgreementExpiration
+import no.nav.hm.grunndata.register.agreement.AgreementRegistrationService
 import no.nav.hm.micronaut.leaderelection.LeaderOnly
 import org.slf4j.LoggerFactory
 
 @Singleton
 @Requires(property = "schedulers.enabled", value = "true")
-open class ProductAgreementScheduler(private val productAgreementRegistrationService: ProductAgreementRegistrationService) {
+open class ProductAgreementScheduler(
+    private val productAgreementRegistrationService: ProductAgreementRegistrationService,
+    private val agreementRegistrationService: AgreementRegistrationService,
+    private val agreementExpiration: AgreementExpiration
+) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(ProductAgreementScheduler::class.java)
@@ -28,7 +36,7 @@ open class ProductAgreementScheduler(private val productAgreementRegistrationSer
     }
 
     @LeaderOnly
-    @Scheduled(cron = "0 30 3 * * *")
+    @Scheduled(cron = "0 */30 * * * *")
     open fun activateProductAgreements() {
         LOG.info("Running product agreement activation scheduler")
         runBlocking {
@@ -39,15 +47,27 @@ open class ProductAgreementScheduler(private val productAgreementRegistrationSer
             )
             LOG.info("Found ${toBePublished.size} product agreements to be published")
             toBePublished.forEach {
-                productAgreementRegistrationService.saveAndCreateEvent(
-                    it.copy(
-                        status = ProductAgreementStatus.ACTIVE,
-                        updated = LocalDateTime.now(),
-                        updatedByUser = "system-publish",
-                        updatedBy = REGISTER
-                    ),
-                    isUpdate = true
-                )
+                val agreement = agreementRegistrationService.findById(it.agreementId)!!
+                if (agreement.agreementStatus == AgreementStatus.ACTIVE && agreement.draftStatus == DraftStatus.DONE) {
+                    productAgreementRegistrationService.saveAndCreateEvent(
+                        it.copy(
+                            status = ProductAgreementStatus.ACTIVE,
+                            updated = LocalDateTime.now(),
+                            updatedByUser = "system-publish",
+                            updatedBy = REGISTER
+                        ),
+                        isUpdate = true
+                    )
+                }
+                else {
+                    productAgreementRegistrationService.saveAndCreateEvent(
+                        it.copy(
+                            status = ProductAgreementStatus.INACTIVE,
+                            expired = agreement.expired,
+                            updated = LocalDateTime.now()
+                        ), isUpdate = true
+                    )
+                }
             }
         }
     }
