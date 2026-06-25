d08b13be905c37be21cd7ee36ea8ad38efc910b4
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2026-06-25 07:52:55 +0200

    more jackson 3 configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
 
     runtimeOnly("org.yaml:snakeyaml")
     implementation("io.micronaut:micronaut-jackson-databind")
+    implementation("tools.jackson.module:jackson-module-kotlin")
 
     // coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
--- src/main/kotlin/no/nav/hm/grunndata/register/JacksonConfig.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/JacksonConfig.kt
@@ -6,9 +6,12 @@ import io.micronaut.context.event.BeanCreatedEvent
 import io.micronaut.context.event.BeanCreatedEventListener
 import jakarta.inject.Singleton
 import tools.jackson.databind.DeserializationFeature
+import tools.jackson.databind.SerializationFeature
 import tools.jackson.databind.cfg.DateTimeFeature
 import tools.jackson.databind.cfg.EnumFeature
+import tools.jackson.databind.introspect.DefaultAccessorNamingStrategy
 import tools.jackson.databind.json.JsonMapper
+import tools.jackson.module.kotlin.kotlinModule
 
 @Singleton
 class JacksonConfig : BeanCreatedEventListener<JsonMapper.Builder> {
@@ -18,16 +21,20 @@ class JacksonConfig : BeanCreatedEventListener<JsonMapper.Builder> {
     }
 
     override fun onCreated(event: BeanCreatedEvent<JsonMapper.Builder>): JsonMapper.Builder {
-        LOG.info("Initialized JacksonConfig")
+        LOG.info("Initialized Jackson 3 config")
         event.bean
-            .changeDefaultPropertyInclusion{it.withValueInclusion(JsonInclude.Include.NON_NULL)}
+            .addModule(kotlinModule())
+            .changeDefaultPropertyInclusion{incl -> incl.withValueInclusion(JsonInclude.Include.NON_NULL)}
+            .changeDefaultPropertyInclusion{incl -> incl.withContentInclusion(JsonInclude.Include.NON_NULL)}
             .disable(DateTimeFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS)
             .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
             .configure(EnumFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true)
-            .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
-            .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .configure(tools.jackson.databind.SerializationFeature.INDENT_OUTPUT, false)
+            .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+            .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
+            .configure(SerializationFeature.INDENT_OUTPUT, false)
+            .accessorNaming(DefaultAccessorNamingStrategy.Provider().withFirstCharAcceptance(true, true))
+            .configure(DeserializationFeature.FAIL_ON_TRAILING_TOKENS, false)
         return event.bean
     }
 }
