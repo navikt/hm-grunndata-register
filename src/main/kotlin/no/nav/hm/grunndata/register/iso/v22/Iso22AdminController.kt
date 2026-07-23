74db2681586073df110f0600b23cb6fee74d4e58
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2026-07-23 08:25:17 +0200

    create read update iso22

--- src/main/kotlin/no/nav/hm/grunndata/register/iso/v22/Iso22AdminController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/iso/v22/Iso22AdminController.kt
@@ -1,13 +1,20 @@
 package no.nav.hm.grunndata.register.iso.v22
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Put
 import io.micronaut.security.annotation.Secured
+import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
 import io.swagger.v3.oas.annotations.tags.Tag
+import kotlinx.coroutines.flow.toList
+import no.nav.hm.grunndata.register.error.BadRequestException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
 
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller(Iso22AdminController.API_V1_ADMIN_ISO22)
@@ -15,8 +22,8 @@ import org.slf4j.LoggerFactory
 class Iso22AdminController(private val iso22Repository: Iso22Repository, private val isoMapRepository: IsoMapRepository) {
 
 
-    @Post("/")
-    suspend fun updateIso22(@Body isos: List<Iso22>) {
+    @Post("/upload")
+    suspend fun uploadList(@Body isos: List<Iso22>) {
         LOG.info("Got iso22: $isos")
         isos.forEach { iso22 ->
             iso22Repository.findByIsoCode(iso22.isoCode)?.let {
@@ -28,7 +35,7 @@ class Iso22AdminController(private val iso22Repository: Iso22Repository, private
     }
 
     @Post("/mapping")
-    suspend fun updateIso22Mapping(@Body mappings: List<IsoMap>) {
+    suspend fun updateIsoMapping(@Body mappings: List<IsoMap>) {
         LOG.info("Got iso22 mappings: $mappings")
         mappings.forEach { mapping ->
             isoMapRepository.findByCode16(mapping.code16!!)?.let {
@@ -39,8 +46,31 @@ class Iso22AdminController(private val iso22Repository: Iso22Repository, private
         }
     }
 
+    @Get("/")
+    suspend fun getAllIsos(): List<Iso22> {
+        return iso22Repository.findAll().toList()
+    }
+
+
+    @Post("/")
+    suspend fun createIso(@Body iso: Iso22, authentication: Authentication): HttpResponse<Iso22> =
+        iso22Repository.findByIsoCode(iso.isoCode)?.let {
+            throw BadRequestException("Iso22 ${iso.isoCode} already exists")
+        } ?: HttpResponse.created(iso22Repository.save(iso.copy(createdByUser = authentication.name,
+            updatedByUser = authentication.name, created = LocalDateTime.now(), updated = LocalDateTime.now())))
+
+
+    @Put("/{isocode}")
+    suspend fun updateIsoByIsocode(isocode: String, @Body iso: Iso22, authentication: Authentication): HttpResponse<Iso22> =
+        iso22Repository.findByIsoCode(isocode)?.let { inDb ->
+            HttpResponse.ok(iso22Repository.update(iso.copy(id = inDb.id, created = inDb.created,
+                createdByUser = inDb.createdByUser, updatedByUser = authentication.name, updated = LocalDateTime.now())))
+        } ?: HttpResponse.notFound()
+
     companion object {
         private val LOG: Logger = LoggerFactory.getLogger(Iso22AdminController::class.java)
         const val API_V1_ADMIN_ISO22 = "/api/v1/admin/iso22"
     }
+
+
 }
