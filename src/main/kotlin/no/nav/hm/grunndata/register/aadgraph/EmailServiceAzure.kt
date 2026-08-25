9c09baa387381006e348f9f3ecccc40f2ced1231
marte pedersen <marte.pedersen@nav.no>
2026-08-25 15:56:28 +0200

    Revert "hente userid først? funker ikke byUserId med epost?"
    This reverts commit 274ee665ba72ce95c6e25e9fa8a16a68e0550fd8.

--- src/main/kotlin/no/nav/hm/grunndata/register/aadgraph/EmailServiceAzure.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/aadgraph/EmailServiceAzure.kt
@@ -59,8 +59,7 @@ open class EmailServiceAzure(private val aadProperties: AzureADProperties) : Ema
         sendMailPostRequestBody.saveToSentItems = true;
 
         kotlin.runCatching {
-            val userId = graphClient.usersWithUserPrincipalName("ikke.svar.finnhjelpemiddel@nav.no").get().id
-            graphClient.users().byUserId(userId).sendMail().post(sendMailPostRequestBody)
+            graphClient.users().byUserId("ikke.svar.finnhjelpemiddel@nav.no").sendMail().post(sendMailPostRequestBody)
         }
             .onSuccess {
                 LOG.info("mail sent")
