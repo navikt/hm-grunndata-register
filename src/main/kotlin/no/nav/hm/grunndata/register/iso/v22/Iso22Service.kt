e22d5746770b71762c686bad277a5a9c9d0d0b26
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2026-08-25 14:08:23 +0200

    improve

--- src/main/kotlin/no/nav/hm/grunndata/register/iso/v22/Iso22Service.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/iso/v22/Iso22Service.kt
@@ -1,5 +1,7 @@
 package no.nav.hm.grunndata.register.iso.v22
 
+
+import io.micronaut.context.annotation.Context
 import jakarta.inject.Singleton
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
@@ -9,14 +11,10 @@ import org.slf4j.LoggerFactory
 @Singleton
 class Iso22Service(
     private val iso22Repository: Iso22Repository,
-    private val isoMapRepository: IsoMapRepository
-
+    private val iso16TreeMigrate: Iso16TreeMigrate,
 ) {
 
     private var iso22Categories: Map<String, Iso22>
-
-
-    var isoMaps: Map<String, IsoMap> = emptyMap()
 
     companion object {
         private val LOG = LoggerFactory.getLogger(IsoCategoryService::class.java)
@@ -25,17 +23,11 @@ class Iso22Service(
     init {
         runBlocking {
             iso22Categories = iso22Repository.findAll().toList().associateBy { it.isoCode }
-            isoMaps = isoMapRepository.findAll()
-                .toList()
-                .filter { it.code16 != null && it.code22 != null }
-                .associateBy { it.code16!! }
+            //iso16TreeMigrate.migrateIso16Tree()
         }
     }
     fun lookUp22Code(iso22Code: String): Iso22? = iso22Categories[iso22Code]
 
     fun retrieveAll22Iso2(): List<Iso22> = iso22Categories.values.toList()
 
-    fun toCode22(code16: String): String? = isoMaps[code16]?.code22
-
-    fun toIsoMap(code16: String): IsoMap? = isoMaps[code16]
 }
