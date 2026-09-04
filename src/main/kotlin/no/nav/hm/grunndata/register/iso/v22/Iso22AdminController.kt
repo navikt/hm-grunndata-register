949cae4f79442f787ea20eb63d8acd0b536f459f
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2026-09-04 15:23:17 +0200

    refactor

--- src/main/kotlin/no/nav/hm/grunndata/register/iso/v22/Iso22AdminController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/register/iso/v22/Iso22AdminController.kt
@@ -19,57 +19,9 @@ import java.time.LocalDateTime
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller(Iso22AdminController.API_V1_ADMIN_ISO22)
 @Tag(name="Admin IsoCategory")
-class Iso22AdminController(private val iso22Repository: Iso22Repository,
-                           private val isoMapRepository: IsoMapRepository,
-                           private val iso16ToIso22Util: Iso16ToIso22Util) {
+class Iso22AdminController(private val iso22Repository: Iso22Repository) {
 
-
-    @Post("/upload")
-    suspend fun uploadList(@Body isos: List<Iso22>) {
-        LOG.info("Got iso22: $isos")
-        isos.forEach { iso22 ->
-            iso22Repository.findByIsoCode(iso22.isoCode)?.let {
-                iso22Repository.update(iso22.copy(id = it.id, created = it.created))
-            } ?:run {
-                iso22Repository.save(iso22)
-            }
-        }
-    }
-
-    @Post("/mapping")
-    suspend fun updateIsoMapping(@Body mappings: List<IsoMap>) {
-        LOG.info("Got iso22 mappings: $mappings")
-        // verify everything is ok before saving, if not throw exception
-        mappings.forEach { mapping ->
-            if (mapping.mapEnum.isEmpty()) {
-                throw BadRequestException("Mapping for code16: ${mapping.code16} and code22: ${mapping.code22} is empty")
-            }
-        }
-        mappings.forEach { mapping ->
-
-            isoMapRepository.findById(mapping.id)?.let {
-                isoMapRepository.update(mapping.copy(id = it.id, created = it.created, verified = mapping.mapEnum.contains(IsoMapEnum.SAME)))
-            } ?: run {
-                isoMapRepository.save(mapping)
-            }
-        }
-    }
-
-    @Post("/nat-mapping")
-    suspend fun updateIsoNatMapping(@Body mappings: List<IsoMap>) {
-        iso16ToIso22Util.rebuildIso16NatTo22Map()
-    }
-
-    @Get("/check-mapping")
-    suspend fun checkIsoMapping() {
-        iso16ToIso22Util.checkCode22Mappings()
-    }
-
-    @Post("/rebuild-iso22-tree")
-    suspend fun rebuildIso22Tree() {
-        iso16ToIso22Util.rebuildIso22Tree()
-    }
-
+    
     @Get("/")
     suspend fun getAllIsos(): List<Iso22> {
         return iso22Repository.findAll().toList()
